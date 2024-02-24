3956e3b13b5ae55bf161242f216303129d27d38c
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-24 20:43:01 +0100

    Create Disp.py

---
+++ Base/Disp.py
@@ -0,0 +1,16 @@
+age = 45
+name = "ibrahim"
+double = 3.4
+is_single = True
+print(age)
+print(name)
+print(double)
+print(is_single)
+print(age + double)
+
+#print(type(name))
+print(type(double))
+print(type(is_single))
+print('Ibrahim' + ' ' + 'Aslam')
+test = f"Name {name}, age {age}, is_single {is_single}. "
+print(test)
