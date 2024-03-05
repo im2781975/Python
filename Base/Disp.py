1f62979c24fa461ac87e392b921149f761c9bce7
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-03-05 15:12:39 +0100

    Update Disp.py

--- Base/Disp.py
+++ Base/Disp.py
@@ -14,3 +14,15 @@ print(type(is_single))
 print('Ibrahim' + ' ' + 'Aslam')
 test = f"Name {name}, age {age}, is_single {is_single}. "
 print(test)
+first = input("First Money: ")
+print("First Money is:",first)
+second = input("Second Money: ")
+print("Second Money is:",second)
+print("First money is: ",first,"Second Money is: ",second)
+#concatenate string
+total = first+second
+first_int=int(first)
+second_int=int(second)
+t = first_int + second_int
+print("Total Money (string) is: ",total)
+print("Total Money (int) is: ",t)
