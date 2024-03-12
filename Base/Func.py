973676a075f4f813dde4137b6b057cc9162b601c
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-03-12 20:27:50 +0100

    Update Func.py

--- Base/Func.py
+++ Base/Func.py
@@ -27,3 +27,28 @@ def all_sum(x, y, *args):
         
 t = all_sum(45, 56, 77, 89)
 print(t)
+
+#def full(title, first, last)
+def full(*title, first, last):
+    name = f'{title} {first} {last}'
+    return name
+def f(first, last, **add):
+    name = f'{first} {last}'
+    print(add['title'])
+    for key,value in add.items():
+        print(key, value)
+#name = full('ibrahim', 'molla')
+#name = full(last = 'molla', first = 'ibrahim')
+#name = full(title = 'DJ', last = 'molla', first = 'ibrahim')
+name = f(title = 'DJ', last = 'molla', first = 'ibrahim')
+#print(name)
+
+def a_lot(a, b):
+    add = a+b
+    mult = a*b
+    remain = a-b
+    return [add, mult, remain]
+    #return add, mult, remain
+res = a_lot(5, 9)
+print(res)
+
