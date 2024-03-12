005ee5ff52ddc0005fd488b8220976f49f4781bb
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-03-12 19:30:34 +0100

    Update Func.py

--- Base/Func.py
+++ Base/Func.py
@@ -5,11 +5,25 @@ def double_it(num):
 double_it(8)
 double_it(9)
 
-def sum(a, b):
-    res = a + b
+def sum(a, b, c = 0):
+    res = a + b + c
     return res
+#def sum(a, b):
+    #res = a + b
+    #return res
 total = sum(23, 24)
 print(total)
 
 final = double_it(total)
 print("Final value is:",final)
+
+def all_sum(x, y, *args):
+    print(args)
+    s = 0
+    for num in args:
+        print(num)
+        s+=num
+    print(s)
+        
+t = all_sum(45, 56, 77, 89)
+print(t)
