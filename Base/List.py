9471fb28ef73747ed7e9d4fa4bc2c7afb65516d2
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-03-13 20:34:28 +0100

    Create List.py

---
+++ Base/List.py
@@ -0,0 +1,13 @@
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(num[3], num[-3])
+#start:end
+print(num[2:6])
+print(num[:])
+print(num[1:])
+print(num[:7])
+#start:end:step
+print(num[1:8:2])
+print(num[1:8:-1])
+print(num[8:1:-2])
+print(num[::-1])
+
