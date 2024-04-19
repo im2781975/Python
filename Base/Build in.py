598cb3c006ff4173a0f25aee86d956043d737d16
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-04-19 15:15:16 +0200

    Create Build in.py

---
+++ Base/Build in.py
@@ -0,0 +1,10 @@
+from math import *
+from random import *
+from time import sleep
+res = ceil(4.0001)
+result = floor(3.9000)
+print(res, result)
+print(random())
+print(randint(1, 100))
+sleep(4)
+print(choice(['A', 'B', 'C', 'D', 'E']))
