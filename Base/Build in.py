35d902c2177ed434000d177af9392456148b4dbb
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-04-19 16:10:07 +0200

    Update Build in.py

--- Base/Build in.py
+++ Base/Build in.py
@@ -1,3 +1,4 @@
+#import pyautogui
 from math import *
 from random import *
 from time import sleep
@@ -8,3 +9,18 @@ print(random())
 print(randint(1, 100))
 sleep(4)
 print(choice(['A', 'B', 'C', 'D', 'E']))
+"""
+sleep(5)
+for i in range(0, 3):
+    pyautogui.write('i am here',interval = 0.25)
+    pyautogui.press('enter')
+"""
+with open('message.txt','w') as file:
+    file.write('python')
+try:
+    x = 45/1
+    print(x)
+except:
+    print("Error happened")
+finally:
+    print("Done")
