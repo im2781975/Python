9fb52599daa0039af5154fafd853e7365d16b06d
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-04-20 07:24:10 +0200

    Update Method.py

--- Base/Method.py
+++ Base/Method.py
@@ -2,5 +2,26 @@ class phone:
     brand = 'xphone'
     price = 2200
     color = 'blue'
+    feature = ['camera', 'speaker', 'hammer']
+    def call(self):
+        print("calling")
+    def send(self,phone, sms):
+        text = f'send message to {phone} & message is {sms}'
+        return text
+class calculator:
+    def add(self, a, b):
+        return a+b
+    def deduct(self, a, b):
+        if a >= b:
+            return a - b
+        else:
+            return b - a
+    def mult(self, a, b):
+        return a * b
+    def div(self, a, b):
+        return a/b
 x = phone()
-print(x.brand, x.price, x.color)
+print(x.brand, x.price, x.color, x.feature)
+x.call()
+result = x.send(23456, 'mollavai')
+print(result)
