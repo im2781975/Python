fcfff431a7b33c64383ab38bcf8ef8f1436f14ca
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-04-20 09:25:25 +0200

    Update Class.py

--- Base/Class.py
+++ Base/Class.py
@@ -8,16 +8,27 @@ class phone:
         text = f'Send from {phone}, call me {sms}'
         print(text)
 class shop:
-    cart =[]
+    class_cart =[]
     def __init__(self,buyer):
         self.buyer= buyer
+        self.init_cart = []
     def add(self,item):
-        self.cart.append(item)
+        self.class_cart.append(item)
+        self.init_cart.append(item)
 a = shop('molla')
 a.add('c')
 a.add('d')
-print(a.cart)
-
+b = shop('vai')
+b.add('e')
+b.add('f')
+c = shop('Aslam')
+c.add('g')
+c.add('h')
+#class attribute will be saved all info but init attribute will be saved specific info
+print(a.class_cart)
+print(a.init_cart)
+print(b.init_cart)
+print(c.init_cart)
 x = phone('A', 'B', 1200)
 print(x.owner, x.brand, x.price)
 x.send('xphone','Dj')
