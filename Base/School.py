3c3fafcdaf8bf75b771f655e87edfa598442224d
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-04-20 13:23:09 +0200

    Update School.py

--- Base/School.py
+++ Base/School.py
@@ -1,9 +1,8 @@
 class Student:
-    def __init__(self, name, id, cur):
+    def __init__(self, name, cur, id):
         self.name = name
-        self.id = id
         self.cur = cur
-        
+        self.id = id
     def __repr__(self) -> str:
         return f'name:{self.name} id:{self.id} cur:{self.cur} '
 
@@ -14,8 +13,7 @@ class Teacher:
         self.id = id
         
     def __repr__(self) -> str:
-        return f'name:{self.name} sub:{self.sub} id:{self.id} '
-
+        return f'name:{self.name} sub:{self.sub} '
 class school:
     def __init__(self, name):
         self.name = name
@@ -34,20 +32,18 @@ class school:
             id = len(self.Students) + 1
             student = Student(name, 'C', id)
             self.Students.append(student)
-            return f'student enrolled with name {name} id {id} extra money {fee - 6500 }'
+            return f"student enrolled with name {self.name} id {self.id} extra money {fee - 6500 }"
 
-    def __repr__(self) -> str:
-        result = f"{self.name}\n"
-        for t in self.Teachers:
-            result += f"{t}\n"
-        for s in self.Students:
-            result += f"{s}\n"
-        return result
-
+    def __rep__(self)->str:
+        print(self.name)
+        for teacher in self.teachers:
+            print(teacher)
+        for student in self.students:
+            print(student)
+            
 a = school('a')
 a.enroll('b', 5200)
 a.enroll('c', 6700)
 a.add('d', 'e')
 a.add('f', 'g')
 print(a)
-
