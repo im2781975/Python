7462fca346590761d2b7b16ec0e73d1ef6a80a6c
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-04-24 16:13:38 +0200

    Create Company.py

---
+++ Base/Company.py
@@ -0,0 +1,25 @@
+class company:
+    def __init(self, name, address):
+        self.name = name
+        self.address = address
+        self.bus = []
+        self.routes = []
+        self.driver = []
+        self.counter = []
+        self.manager = []
+        self.supervisior = []
+        self.fair = []
+class Driver:
+    def __init(self, licence, name, age):
+        self.licence = licence
+        self.name = name
+        self.age = age
+class Counter:
+    def __ init__(self):
+        pass
+    def __init__(self, from, to):
+        pass
+class passenger:
+    pass
+class supervisior:
+    pass
