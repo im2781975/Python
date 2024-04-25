07cb34e4dd7d59d30a9299d9252f4c13ea939040
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-04-25 13:57:18 +0200

    Update Polymarphism.py

--- Base/Polymarphism.py
+++ Base/Polymarphism.py
@@ -1,3 +1,23 @@
+from math import pi
+class shape:
+    def __init__(self, name):
+        self.name = name
+class rect(shape):
+    def _init__(self, name, length, width):
+        self.length = length
+        self.width = width
+        super().__init__(name)
+    def area(self):
+        return self.length * self.width
+class circle(shape):
+    def __init__(self, name, radius):
+        self.radius = radius
+        super().__init__(name)
+    def area(self):
+        return self.radius * pi
+x = circle('x', 5)
+print(x.area())
+"""
 class animal:
     def __init__(self, name):
         self.name = name
@@ -24,3 +44,4 @@ z = goat("z")
 ani = [x, y, z]
 for a in ani:
     a.sound()
+"""
