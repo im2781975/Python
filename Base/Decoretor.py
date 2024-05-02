f956d7bc544d6dc49eb0494d3aa107bd50c36e8c
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-05-02 20:03:12 +0200

    Update and rename Inner.py to Decoretor.py

---
+++ Base/Decoretor.py
@@ -0,0 +1,43 @@
+import time
+def double_dacker():
+    print("Double Dacker")
+    def inner():
+        print("inner")
+        return 3000
+    return inner
+
+print(double_dacker())
+print(double_dacker()())
+
+def do_something(work):
+    print("Starting")
+    #print(work)
+    #for passing function as a parameter have to use function
+    work()
+    print("Ended")
+def coding():
+    print("Coded")
+#do_something(2)
+do_something(coding)
+
+def timer(func):
+    start = time.time()
+    def outer(*args,**kargs):
+        print("Started")
+        func(*args,**kargs)
+        print("ended")
+        end = time.time()
+        print({end - start})
+    return outer
+import math
+@timer
+def fact(n):
+    print("Factorial")
+    res = math.factorial(n)
+    print(res)
+#timer(fact(5))()
+fact(n = 120)
+"""@timer
+def out():
+    print("Going")
+out() """
