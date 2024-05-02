a5eaca4fecfbafcd9183972975ea81169ac4a759
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-05-02 17:11:46 +0200

    Update Property.py

--- Base/Property.py
+++ Base/Property.py
@@ -1,3 +1,4 @@
+#decorator can modify a function in attribute
 class user:
     def __init__(self, name, age, money):
         self._name = name
@@ -5,10 +6,23 @@ class user:
         self.__money = money
     def age(self):
         return self._age
+    # getter without any setter is read only attribute
     @property
     def name(self):
-        return self.name
+        return self._name
+    @property
+    def salary(self):
+        return self.__money
+    #setter
+    @salary.setter
+    def salary(self, val):
+        if val < 0:
+            return f'Can\' negetive'
+        self.__money += val
 a = user('x', 12, 1223)
 print(a._age)#used as attribute
 print(a.age())#used as method
 print(a.name)#used as attribute
+print(a.salary)
+a.salary = 200
+print(a.salary)
