cd80443f08d20185103448a1c971df115293b30a
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-05-11 11:37:52 +0200

    Update Ride.py

--- Base/Ride.py
+++ Base/Ride.py
@@ -1,5 +1,5 @@
-
 from abc import ABC,abstractmethod
+from datetime import datetime
 class user:
     def __init__(self, name, email, nid):
         self.name = name
@@ -13,7 +13,11 @@ class user:
 class rider(user):
     def __init__(self, name, email, nid):
         self.cur_ride = None
+        self.wallet = 0
         super().__init__(name, email, nid)
+    def load_cash(self, amount):
+        if amount > 0:
+            wallet += amount
     def display(self):
         print(f'{self.name} {self.email} {self.nid}')
     def request_ride(self, location, destination):
@@ -26,8 +30,26 @@ class driver(user):
     def __init__(self, name, email, nid, cur_loc):
         super().__init__(name, email, nid):
         self.cur_loc = cur_loc
+        self.wallet = 0
     def display(self):
         print(f'{self.name} {self
         .email} {self.nid}')
     def accept_ride(self, ride):
         ride.set_driver(self)
+class Ride:
+    def __init__(self, start_loc, end_loc):
+        self.start_loc = start_loc
+        self.end_loc = end_loc
+        self.start_time = None
+        self.end_time = None
+        self.driver = None
+        self.rider = None
+        self.estimated_fare = None
+    def set_driver(self, driver):
+        self.set_driver = driver
+    def start_ride(self):
+        self.start_time = datetime.now()
+    def end_ride(self, rider, amount):
+        self.end_time = datetime.now()
+        self.rider.wallet -= estimated_fare
+        self.driver.wallet += estimated_fare
