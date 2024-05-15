f06450c64eb9e0412a853058adfb81a038de19b7
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-05-15 11:41:21 +0200

    Update shool.py

--- Base/shool.py
+++ Base/shool.py
@@ -15,6 +15,51 @@ class school:
             self.classrooms[classname].add_student(student)
         else:
             print(f'{classname} isn\'t available ')
+    @staticmethod
+    def grade_calculate(marks):
+        if 80 <= marks <= 100:
+            return 'A+'
+        elif 70 <= marks <= 80:
+            return 'A'
+        elif 60 <= marks <= 70:
+            return 'A-'
+        elif 50 <= marks <= 60:
+            return 'B'
+        elif 40 <= marks <= 50:
+            return 'C'
+        elif 33 <= marks <= 40:
+            return 'D'
+        else:
+            return 'F'
+    
+    @staticmethod
+    def grade_to_val(grade):
+        grade_map = {
+            'A+' : 5.00,
+            'A' : 4.00,
+            'A-' : 3.50,
+            'B' : 3.00,
+            'C' : 2.00,
+            'D' : 1.00,
+            'E' : 0.00
+        }
+        return grade_map[grade]
+    @staticmethod
+    def value_to_grade(value):
+        if 4.50 <= value <= 5.00:
+            return 'A+'
+        elif 3.50 <= value < 4.50:
+            return 'A'
+        elif 3.00 <= value < 3.50:
+            return 'A-'
+        elif 2.50 <= value < 3.00:
+            return 'B'
+        elif 2.00 <= value < 2.50:
+            return 'C'
+        elif 1.00 <= marks <= 2.00:
+            return 'D'
+        else:
+            return 'F'
     def __repr__(self)->str:
         for key, value in self.classrooms.items():
             print(key)
@@ -25,7 +70,7 @@ class school:
             print(subject.name,subject.teacher.name)
         for student in eight.students:
             for key, value in student.marks.items():
-                print(student.name, key, value)
+                print(student.name, key, value, student.subject_grade[key])
         return ''
 class ClassRoom:
     def __init__(self, name):
@@ -41,6 +86,8 @@ class ClassRoom:
     def take_final(self):
         for sub in self.subjects:
             sub.exam(self.students)
+        for student in self.students:
+            student.calculate_final_grade()
     def __str__(self):
         return f'{self.name} - {len(self.students)} '
     def get_top_students(self):
@@ -56,6 +103,7 @@ class Subject:
         for student in students:
             mark = self.teacher.take_exam()
             student.marks[self.name] = mark
+            student.subject_grade[self.name] = school.grade_calculate(mark)
 class Person:
     def __init__(self, name):
         self.name = name
@@ -76,7 +124,14 @@ class Student(Person):
         self.classroom = classroom
         self.subjects = []
         self.marks = {}
+        self.subject_grade = {}
         self.grade = None
+    def calculate_final_grade(self):
+        sum = 0
+        for grade in self.subject_grade.values():
+            point = school.grade_to_val(grade)
+            sum += point
+            print(self.name, grade, point)
     @property
     def id(self):
         return self.__id
