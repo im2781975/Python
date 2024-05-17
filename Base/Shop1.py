a2070ecf149261ae6aca00deff58393a6bff765b
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-05-17 21:56:09 +0200

    Create Shop1.py

---
+++ Base/Shop1.py
@@ -0,0 +1,25 @@
+class Product:
+    def __init__(self, name, price, quantity):
+        self.product_name = name
+        self.product_price = price
+        self.product_quantity = quantity
+class Store:
+    def __init__(self):
+        self.__products_price = {}
+        self.__products_quantity = {}
+    def add_product(self, name, price, quantity):
+        product = Product(name, price, quantity)
+        self.__products_price[product.product_name] = product.product_price
+        self.__products_quantity[product.product_name] = product.product_quantity
+    @property
+    def show(self):
+        print(self.__products_price)
+        print(self.__products_quantity)
+class Shop(Store):
+    def __init__(self, name):
+        self.shop_name = name
+        super().__init__()
+x = Shop("xyz")
+x.add_product("abc", 400, 15)
+x.add_product("bcd", 200, 20)
+x.show
