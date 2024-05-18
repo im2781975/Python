3b25ad374f1be0ff05f69f82b29da5c646828aa2
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-05-18 18:08:19 +0200

    Update Shop1.py

--- Base/Shop1.py
+++ Base/Shop1.py
@@ -11,6 +11,13 @@ class Store:
         product = Product(name, price, quantity)
         self.__products_price[product.product_name] = product.product_price
         self.__products_quantity[product.product_name] = product.product_quantity
+    def buy_product(self, name, quantity):
+        if name in self.__products_price:
+            if self.__products_quantity[name] >= quantity:
+                self.__products_quantity[name] -= quantity
+                
+        else:
+            print("Not Found")
     @property
     def show(self):
         print(self.__products_price)
@@ -22,4 +29,6 @@ class Shop(Store):
 x = Shop("xyz")
 x.add_product("abc", 400, 15)
 x.add_product("bcd", 200, 20)
+x.show
+x.buy_product("abc", 2)
 x.show
