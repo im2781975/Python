c39b77aa837ec42613e9657541809915c71d2525
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-05-19 11:13:19 +0200

    Update Bus company.py

--- Base/Bus company.py
+++ Base/Bus company.py
@@ -14,7 +14,7 @@ class bus:
 b = bus(2, "abc", "13.00", "13.30", "Dhaka", "barisal")
 class phitron:
     total_bus = 5
-    total_bus_list = [{}, {}]
+    total_bus_list = []
     def add_bus(self):
         bus_no = int(input("Enter:"))
         flag = 1
@@ -27,11 +27,25 @@ class phitron:
             bus_driver = input("Name: ")
             bus_arrival = input("Arrival time: ")
             bus_depart = input("Depart time: ")
-            bus_from = input("From dst")
+            bus_from = input("From dst: ")
             bus_to = input("To dst: ")
             self.new_bus = bus(bus_no, bus_driver, bus_arrival, bus_depart, bus_from, bus_to)
             self.total_bus_list.append(vars(self.new_bus))
             print("Added successfully")
+class counter(phitron):
+    def reservation(self):
+        bus_no = int(input("Enter: "))
+        for w in self.total_bus_list:
+            if bus_no == w['coach']:
+                passenger = input("Name: ")
+                seat_no = int(input("seat_no: "))
+                if seat_no > 20:
+                    print("No seats avaiable")
+                elif w['seat'][seat_no - 1]!= "Empty":
+                    print("Already booked")
+                else:
+                    w['seat'][seat_no -1] = passenger
+                    
 #print(vars(b))
 company = phitron()
 company.add_bus()
