bdeedeafe60f5c7a4fc8dc3500d8b56606299881
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-05-27 10:42:10 +0200

    Update First app.py

--- Django/Page Navigate/First app.py
+++ Django/Page Navigate/First app.py
@@ -9,6 +9,12 @@ urlpatterns = [
 from django.shortcuts import render
 from django.http import HttpResponse
 def contact(request):
-    return HttpResponse("This is contact page")
+    return HttpResponse('''
+    <h1>this is contact page <\h1>
+    <a href = '/first_app/about/'>About</a>
+    ''')
 def about(request):
-    return HttpResponse("This is about page")
+    return HttpResponse('''
+    <h1>this is about page <\h1>
+    <a href = '/first_app/contact/'>Contact</a>
+    ''')
