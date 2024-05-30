f3effa7e2f125f6ed22b83e751cb80ec47efa7e8
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-05-30 14:48:44 +0200

    Create Conversion.py

---
+++ Intro/Conversion.py
@@ -0,0 +1,11 @@
+x = 20
+y = 10.2
+print(x + y)
+print(type(x))
+print(0o123, 0O123)
+#converted octal to decimal to decimal
+print(0b101, 0B101)
+#converted binary to decimal to decimal
+print(0x87, 0X87)
+#converted hexa to decimal to decimal
+print(type(0x87))
