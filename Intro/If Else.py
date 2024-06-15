cf5d24f0544c62a5c3194c5e421cfe4a74e9188a
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-15 08:50:51 +0200

    Create If Else.py

---
+++ Intro/If Else.py
@@ -0,0 +1,11 @@
+height = int(input("Enter height: "))
+if height > 3:
+    print("Token required")
+    print("Board metro")
+else:
+    print("No token required")
+num = int(input("Enter number: "))
+if num % 2==0:
+    print("Even")
+else:
+    print("Odd")
