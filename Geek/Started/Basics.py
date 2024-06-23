35adcdf46c9693dc73019345732e30dd15af0ee8
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-23 21:32:50 +0200

    Create Basics.py

---
+++ Geek/Started/Basics.py
@@ -0,0 +1,161 @@
+x = "Hello World" # string 
+x = 50  # integer 
+x = 60.5  # float 
+x = 3j  # complex 
+x = ["geeks", "for", "geeks"]  # list  
+x = ("geeks", "for", "geeks")  # tuple 
+x = {"name": "Suraj", "age": 24} # dict 
+x = {"geeks", "for", "geeks"} # set 
+x = True  # bool 
+x = b"Geeks" # binary 
+
+//
+a = 9
+
+b = 4
+
+add = a + b  
+
+  
+
+sub = a - b  
+
+  
+
+mul = a * b  
+
+  
+
+mod = a % b  
+
+  
+
+p = a ** b  
+
+print(add)  
+
+print(sub)  
+
+print(mul)  
+
+print(mod)  
+
+print(p)  
+#logical 
+a = True
+
+b = False
+
+print(a and b)  
+
+print(a or b)  
+
+print(not a)
+#Bitwise
+a = 10
+
+b = 4
+
+print(a & b)  
+
+print(a | b)  
+
+print(~a)  
+
+print(a ^ b)  
+
+print(a >> 2)  
+
+print(a << 2) 
+#assingment 
+a = 10
+
+b = a  
+
+print(b)  
+
+b += a  
+
+print(b)  
+
+b -= a  
+
+print(b)  
+
+b *= a  
+
+print(b)  
+
+b <<= a  
+
+print(b) 
+#if else
+i = 20
+
+if (i < 15):  
+
+    print("i is smaller than 15")  
+
+    print("i'm in if Block")  
+
+else:  
+
+    print("i is greater than 15")  
+
+    print("i'm in else Block")  
+
+print("i'm not in if and not in else Block")  
+#if_ elif
+i = 20
+
+if (i == 10):  
+
+    print("i is 10")  
+
+elif (i == 15):  
+
+    print("i is 15")  
+
+elif (i == 20):  
+
+    print("i is 20")  
+
+else:  
+
+    print("i is not present")
+#for loop
+for i in range(0, 10, 2):  
+
+    print(i)  
+#while loop
+# Python program to illustrate while loop  
+
+count = 0
+
+while (count < 3):  
+
+    count = count + 1
+
+    print("Hello Geek") 
+#Find even odd
+# A simple Python function to check 
+# whether x is even or odd 
+
+def evenOdd(x): 
+
+    if (x % 2 == 0): 
+
+        print("even") 
+
+    else: 
+
+        print("odd") 
+
+  
+
+  
+# Driver code to call the function 
+
+evenOdd(2) 
+
+evenOdd(3) 
