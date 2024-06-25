0b1ed6da8f8e89dbb3ff128158ab2076bd07fafe
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-25 10:35:55 +0200

    Update print().py

--- Geek/Input/print().py
+++ Geek/Input/print().py
@@ -47,4 +47,49 @@ print(a,b,c,sep="-")
 #-----
 print(10, 20, sep=' - ', 30)
 #----
+# Python 2 code for printing
+# on the same line printing
+# geeks and geeksforgeeks 
+# in the same line
+ 
+# Without newline
+print("geeks"),
+print("geeksforgeeks")
+ 
+# Array
+a = [1, 2, 3, 4]
+ 
+# Printing each element on the same line
+for i in xrange(4):
+    print(a[i]),
+#--++--
+# Python 3 code for printing
+# on the same line printing 
+# geeks and geeksforgeeks 
+# in the same line
+ 
+print("geeks", end =" ")
+print("geeksforgeeks")
+ 
+# array
+a = [1, 2, 3, 4]
+ 
+# printing a element in same
+# line
+for i in range(4):
+    print(a[i], end =" ") 
+#----
+# Print without newline in Python 3.x without using for loop
+ 
+l = [1, 2, 3, 4, 5, 6]
+ 
+# using * symbol prints the list
+# elements in a single line
+print(*l)
+#-----
+import sys
+ 
+sys.stdout.write("GeeksforGeeks ")
+sys.stdout.write("is best website for coding!")
+#++++
 
