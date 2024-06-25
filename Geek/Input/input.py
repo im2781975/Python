eb343334f120776bd480db11ae87621f12829d4e
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-25 11:26:15 +0200

    Update input.py

--- Geek/Input/input.py
+++ Geek/Input/input.py
@@ -51,4 +51,66 @@ time_str = input("Enter a time (HH:MM:SS): ")
 # Convert string to datetime object
 time_obj = datetime.datetime.strptime(time_str, "%H:%M:%S")
 #----
+# Python program showing how to
+# multiple input using split
+
+# taking two inputs at a time
+x, y = input("Enter two values: ").split()
+print("Number of boys: ", x)
+print("Number of girls: ", y)
+
+# taking three inputs at a time
+x, y, z = input("Enter three values: ").split()
+print("Total number of students: ", x)
+print("Number of boys is : ", y)
+print("Number of girls is : ", z)
+
+# taking two inputs at a time
+a, b = input("Enter two values: ").split()
+print("First number is {} and second number is {}".format(a, b))
+
+# taking multiple inputs at a time 
+# and type casting using list() function
+x = list(map(int, input("Enter multiple values: ").split()))
+print("List of students: ", x)
+#----
+#Example with integers
+a, b, c = map(int, input("Enter three integers separated by spaces: ").split())
+print("Sum:", a + b + c)
+
+#Example with floats:
+x, y = map(float, input("Enter two floats separated by a space: ").split())
+print("Product:", x * y)
+
+#Example with strings:
+name, age = input("Enter your name and age separated by a space: ").split()
+print("Hello,", name + "! You are", age, "years old.")
+#-----
+# Python program showing
+# how to take multiple input
+# using List comprehension
+
+# taking two input at a time
+x, y = [int(x) for x in input("Enter two values: ").split()]
+print("First Number is: ", x)
+print("Second Number is: ", y)
+
+# taking three input at a time
+x, y, z = [int(x) for x in input("Enter three values: ").split()]
+print("First Number is: ", x)
+print("Second Number is: ", y)
+print("Third Number is: ", z)
+
+# taking two inputs at a time
+x, y = [int(x) for x in input("Enter two values: ").split()]
+print("First number is {} and second number is {}".format(x, y))
+
+# taking multiple inputs at a time 
+x = [int(x) for x in input("Enter multiple values: ").split()]
+print("Number of list is: ", x) 
+#-----
+# taking multiple inputs at a time separated by comma
+x = [int(x) for x in input("Enter multiple value: ").split(",")]
+print("Number of list is: ", x) 
+#----
 
