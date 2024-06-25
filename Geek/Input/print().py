169cd609a6ede9ff4700e3f13e928acf963d1e66
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-25 11:14:36 +0200

    Update print().py

--- Geek/Input/print().py
+++ Geek/Input/print().py
@@ -92,4 +92,26 @@ import sys
 sys.stdout.write("GeeksforGeeks ")
 sys.stdout.write("is best website for coding!")
 #++++
+#-----
+# ends the output with a space
+print("Welcome to", end = ' ')
+print("GeeksforGeeks", end= ' ')
+#---
+# ends the output with '@'
+print("Python", end='@')
+print("GeeksforGeeks")
+#---
+print('G','F', sep='', end='')
+print('G')
+#\n provides new line after printing the year
+print('09','12','2016', sep='-', end='\n')
+  
+print('Red','Green','Blue', sep=',', end='@')
+print('geeksforgeeks')
+#----
+name = "Alice"
+age = 30
+print("My name is", name, "and I am", age, "years old.", end=" ")
+print("Nice to meet you!")
+#----
 
