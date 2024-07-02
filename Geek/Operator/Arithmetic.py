a922dbc2b99c62e98291b493c9fd95aebaac5e29
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-02 23:23:36 +0200

    Create Arithmetic.py

---
+++ Geek/Operator/Arithmetic.py
@@ -0,0 +1,47 @@
+val1 = 2
+val2 = 3
+
+# using the addition operator
+res = val1 + val2
+print(res)
+//
+val1 = 2
+val2 = 3
+
+# using the subtraction operator
+res = val1 - val2
+print(res)
+///
+val1 = 2
+val2 = 3
+
+# using the multiplication operator
+res = val1 * val2
+print(res)
+///
+val1 = 3
+val2 = 2
+
+# using the division operator
+res = val1 / val2
+print(res)
+///
+val1 = 3
+val2 = 2
+
+# using the floor division
+res = val1 // val2
+print(res)
+///
+val1 = 3
+val2 = 2
+
+# using the modulus operator
+res = val1 % val2
+print(res)
+///
+val1 = 2
+val2 = 3
+# using the exponentiation operator
+res = val1 ** val2
+print(res)
