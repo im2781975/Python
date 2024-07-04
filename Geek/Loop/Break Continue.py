aa2b5c885bd52d3c777d36a2461fa39c39835772
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-05 00:32:55 +0200

    Create Break Continue.py

---
+++ Geek/Loop/Break Continue.py
@@ -0,0 +1,89 @@
+# Python program to demonstrate 
+# break statement 
+  
+s = 'geeksforgeeks'
+# Using for loop 
+for letter in s: 
+  
+    print(letter) 
+    # break the loop as soon it sees 'e' 
+    # or 's' 
+    if letter == 'e' or letter == 's': 
+        break
+  
+print("Out of for loop") 
+print() 
+  
+i = 0
+  
+# Using while loop 
+while True: 
+    print(s[i]) 
+  
+    # break the loop as soon it sees 'e' 
+    # or 's' 
+    if s[i] == 'e' or s[i] == 's': 
+        break
+    i += 1
+  
+print("Out of while loop")
+////
+# Python program to demonstrate 
+# break statement with nested 
+# for loop 
+  
+# first for loop 
+for i in range(1, 5): 
+    
+    # second for loop 
+    for j in range(2, 6): 
+        
+        # break the loop if 
+        # j is divisible by i 
+        if j%i == 0: 
+            break
+              
+        print(i, " ", j)
+////
+# Python program to 
+# demonstrate continue 
+# statement 
+  
+# loop from 1 to 10 
+for i in range(1, 11): 
+  
+    # If i is equals to 6, 
+    # continue to next iteration 
+    # without printing 
+    if i == 6: 
+        continue
+    else: 
+        # otherwise print the value 
+        # of i 
+        print(i, end = " ")
+////
+# Python program to demonstrate 
+# pass statement 
+  
+s = "geeks"
+  
+# Empty loop 
+for i in s: 
+    # No error will be raised 
+    pass
+  
+# Empty function 
+def fun(): 
+    pass
+  
+# No error will be raised 
+fun() 
+  
+# Pass statement 
+for i in s: 
+    if i == 'k': 
+        print('Pass executed') 
+        pass
+    print(i) 
+////
+
