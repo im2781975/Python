66859b9208fef4aafedfaf0328cbd198fac5aeae
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-05 23:57:26 +0200

    Update Execise.py

--- Intro/Execise.py
+++ Intro/Execise.py
@@ -6,3 +6,18 @@ if side == 1:
     print("Tail")
 else:
     print("Head")
+#who pay bill
+import random
+l = ["Ab", "Bc", "Cd", "De"]
+x = random.choice(l)
+
+#splited txt
+text = "Ab Bc Cd De"
+splited_txt = text.split(" ")
+print(splited_txt)
+
+name = input("Enter name separated by space: ")
+splited = name.split(" ")
+length = len(splited)
+x = random.randint(0, length -1)
+print(f"{splited[x]} will pay the bill ")
