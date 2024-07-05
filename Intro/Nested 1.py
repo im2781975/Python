c0fb18b4b2558e6e6bfee287b81836b94ace9c46
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-05 16:22:56 +0200

    Update Nested 1.py

--- Intro/Nested 1.py
+++ Intro/Nested 1.py
@@ -25,3 +25,26 @@ if add_cheese == 'Y' or add_cheese == 'y':
     else:
         bill += 50
 print(f"Final bill is {bill}")
+#count Occurance
+name1 = input("Enter string: ")
+name2 = input("Enter string2: ")
+combine = name1 + name2
+convert_lower = combine.lower()
+t = convert_lower.count('t')
+r = convert_lower.count('r')
+u = convert_lower.count('u')
+e = convert_lower.count('e')
+true = t + r + u + e
+l = convert_lower.count('l')
+o = convert_lower.count('o')
+v = convert_lower.count('v')
+e = convert_lower.count('e')
+love = l + o + v + e
+score = int(str(true) + str(love))
+if(score < 10 or score > 90):
+    print(f"Score is: {score} which between 10 & 90")
+elif(score <= 40 or score >= 60):
+    print(f"Score is: {score} which between 40 & 60")
+else:
+    print(f"Score is {score}")
+    
