7905eb5936f3156b62d9ca14b6e1ecf89e256368
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-05 08:55:30 +0200

    Create pass.py

---
+++ Geek/Function/pass.py
@@ -0,0 +1,18 @@
+# Correct way of writing empty function  
+# in Python 
+def fun():  
+    pass
+////
+# Empty loop in Python 
+mutex = True
+while (mutex == True) : 
+    pass
+////
+# Empty in if/else in Python 
+mutex = True
+if (mutex == True) : 
+    pass
+else : 
+    print("False") 
+////
+
