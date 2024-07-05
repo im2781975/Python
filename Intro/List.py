a9d611918630a2d306ee00e1a56a190601c04f20
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-05 17:50:49 +0200

    Create List.py

---
+++ Intro/List.py
@@ -0,0 +1,8 @@
+
+num = [10, 0, -1, 4]
+name = ["Ab", "Cd"]
+mix_list = [3, "Ef", True, 10.87]
+print(num, name, mix_list)
+print(num[2], num[-1])
+print(num[0:4], num[0:], num[:4], num[0:4:2], num[4:0], num[4:0:-1])
+print(len(num))
