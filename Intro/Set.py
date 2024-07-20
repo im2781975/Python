a6bc5c05d5ed4f17c36aa3041f2c16f8cfa432a8
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-20 09:33:31 +0200

    Update Set.py

--- Intro/Set.py
+++ Intro/Set.py
@@ -1,4 +1,4 @@
-set1 = {10, 56, 89, 90, True, 'Molla', 1};
+set1 = {10, 56, 89, 90, True, 'Molla', 1}
 #1 & True return same value
 set1.add(99)
 set1.remove(90)
@@ -16,6 +16,21 @@ set3 = set()
 print(type(set1), type(set2), type(set3))
 set1 = {1, 2, 3, 4 }
 set2 = {3, 4, 5, 6 }
+set3 = set()
 print(f"Union is: {set1.union(set2)} ")
 #print(f"Union is: {set2.union(set1)} ")
 #print(f"Union is: {set1 | set2} ")
+print(set1.union((10, 11)))
+set1.update(set2)
+#set1|= set2
+set1.update([29, 30])
+print(set1)
+
+print(set1.intersection(set2, set3))
+#print(set1.intersection([19, 20]))
+#print(set1.intersection(set2))
+#print(set1 & set2)
+#print(set1 & set2 & set3)
+
+set1.intersection_update(set2)
+print(set1)
