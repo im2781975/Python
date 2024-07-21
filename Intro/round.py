3dd78a0f6fa57adffc437c2824fff3284074afb3
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-21 10:02:09 +0200

    Update round.py

--- Intro/round.py
+++ Intro/round.py
@@ -12,3 +12,22 @@ print(round(-1.5, 2))
 print(round(675.678, -1))
 print(type(round(6.7777, 2)))
 print(type(round(6.77777)))
+
+#range
+"""for var_name in range(i, j, k)
+i->start, j->stop, k->step
+here i & k is optional"""
+a = range(5)
+print(a[1])
+a = range(2, 10, 2)
+#print(a[1])
+for i in a:
+    print(i, a)
+a = range(10, 0, -1)
+for i in a:
+    print(i)
+total = 0
+for i in range(0, 100, 2):
+    #exclude 100
+    total += i
+print(total)
