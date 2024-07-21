c21e936f6d2fa68196291c1ac61753df6103f5bb
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-21 11:08:09 +0200

    Update Execise 1.py

--- Intro/Execise 1.py
+++ Intro/Execise 1.py
@@ -1,3 +1,4 @@
+#password Generator
 import string, random
 letter = list(string.ascii_lowercase) + list(string.ascii_uppercase)
 letters =list(string.ascii_letters)
@@ -25,3 +26,22 @@ Generated = ""
 for i in password:
     Generated += i
 print(Generated)
+
+#sum of the even num from 0 to 100
+total = 0
+#for i in range(0, 101, 2):
+    #total += i 
+for i in range(0, 101):
+    if i % 2 == 0:
+        total += i
+print(total) 
+#print numbers range(0, 100) where divisible by 3, 5, 3 and 5. instead of i print frizz, buzz, frizzbuzz
+for i in range(101):
+    if i % 3 == 0 and i % 5!= 0:
+        print(i, "Fizz")
+    elif i % 5 == 0 and i % 3 !=0:
+        print(i, "Buzz")
+    elif i % 3 == 0 and i % 5 == 0:
+        print(i, "FizzBuzz")
+    else:
+        print(i)
