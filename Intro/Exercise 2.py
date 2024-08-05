1a800796e15eedc8000bbc4a8b10f1e787da0f30
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-08-05 09:30:00 +0200

    Update Exercise 2.py

--- Intro/Exercise 2.py
+++ Intro/Exercise 2.py
@@ -34,4 +34,24 @@ while not EndOfBidding:
     if MoreBidder == 'no':
         EndOfBidding = True
         FindWinner(BidderData)
-
+#Leap year Day count
+def LeapYear(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+def DaysInMonth(year, month):
+    DaysList = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if LeapYear(year) and month == 2:
+        return 29
+    else:
+        return DaysList[month - 1]
+year = int(input("Enter Year: "))
+month = int(input("Enter Month: "))
+print(DaysInMonth(year, month))
