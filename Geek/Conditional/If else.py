1cbbfb5b2708bfd6fadaf33c1f5622382ceb6dd5
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-12-04 14:52:36 +0100

    Update If else.py

--- Geek/Conditional/If else.py
+++ Geek/Conditional/If else.py
@@ -95,3 +95,59 @@ x = 0
 
 {x > 0: print("Positive"), x < 0: print("Negative")}.get(True, print("Zero"))
 ////
+# if statement example 
+if 10 > 5: 
+    print("10 greater than 5") 
+  
+print("Program ended") 
+////
+# if..else statement example 
+x = 3
+if x == 4: 
+    print("Yes") 
+else: 
+    print("No") 
+////
+# if..else chain statement 
+letter = "A"
+  
+if letter == "B": 
+    print("letter is B") 
+  
+else: 
+  
+    if letter == "C": 
+        print("letter is C") 
+  
+    else: 
+  
+        if letter == "A": 
+            print("letter is A") 
+  
+        else: 
+            print("letter isn't A, B and C")
+////
+# Nested if statement example 
+num = 10
+  
+if num > 5: 
+    print("Bigger than 5") 
+  
+    if num <= 15: 
+        print("Between 5 and 15") 
+////
+# if-elif statement example 
+letter = "A"
+  
+if letter == "B": 
+    print("letter is B") 
+  
+elif letter == "C": 
+    print("letter is C") 
+  
+elif letter == "A": 
+    print("letter is A") 
+  
+else: 
+    print("letter isn't A, B or C")
+////
