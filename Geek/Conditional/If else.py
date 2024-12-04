e3efa4d54eb279b15e57c7a372d1c996ad66e9c7
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-12-04 14:51:45 +0100

    Update If else.py

--- Geek/Conditional/If else.py
+++ Geek/Conditional/If else.py
@@ -74,3 +74,24 @@ i = 10
 print(True) if i < 15 else print(False)
 ////
 
+x = 0
+
+# traditional python if elif else statement
+if x > 0:
+    print("Positive")
+elif x < 0:
+    print("Negative")
+else:
+    print("Zero")
+////
+x = 0
+
+# Python one liner if elif else statement
+result = {x > 0: "Positive", x < 0: "Negative"}.get(True, "Zero")
+
+print(result)
+/////
+x = 0
+
+{x > 0: print("Positive"), x < 0: print("Negative")}.get(True, print("Zero"))
+////
