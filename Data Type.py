f874508896d16e85d89c21d8eff70c08e0375066
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-12-05 20:04:46 +0100

    Update Data Type.py

--- Data Type.py
+++ Data Type.py
@@ -66,8 +66,10 @@ print(repr(today))
 
 #class
 class Represent(object):    
-    def __init__(self, x, y):        self.x, self.y = x, y    
-    def __repr__(self):        return "Represent(x={},y=\"{}\")".format(self.x, self.y)   
+    def __init__(self, x, y):       
+        self.x, self.y = x, y    
+    def __repr__(self):       
+        return "Represent(x={},y=\"{}\")".format(self.x, self.y)   
     def __str__(self):        
         return "Representing x as {} and y as {}".format(self.x, self.y)
 r = Represent(1, "Hopper") 
