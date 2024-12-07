71fad719ce70fda28fb128ddf11421e55fd097d5
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-12-07 19:20:45 +0100

    Create Set.py

---
+++ DS/Set.py
@@ -0,0 +1,17 @@
+A = {1, 2, 3, 4, 5}
+B = {3, 4, 5, 6}
+print(A.intersection(B))
+#print(A & B)
+print(A.union(B))
+#print(A | B)
+print(A.difference(B))
+#print(A - B)
+print(A.symmetric_difference(B))
+#print(A ^ B)
+print(A.issuperset(B))
+print(A >= B)
+print(A.issubset(B))
+print(A <= B)
+print(A.isdisjoint(B))
+
+
