3d0d18df6018087da9b5f8a21902107f0db78596
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-12-08 17:13:12 +0100

    Update Operator.py

--- DS/Operator.py
+++ DS/Operator.py
@@ -19,3 +19,30 @@ a <<= 2
 a >>= 2
 a ^= 0b011
 print(a)
+
+#Boolean
+def or_(a, b):    
+    if a:        
+        return a    
+    else:        
+        return b
+def and_(a, b):    
+    if not a:        
+        return a    
+    else:        
+        return b
+print(or_(10, 15))
+
+def T():
+    return True
+def F():
+    return False
+print(T() and F())
+print(T() or F())
+
+#compare
+x = 3.141
+if 3.14 < x < 3.142:    
+    print("x is near pi")
+else:
+    print("Outer")
