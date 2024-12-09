a2b358f8cb0fa90380fd825e2eb2dcf532b44fb7
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-12-09 19:16:41 +0100

    Update Dict.py

--- DS/Dict.py
+++ DS/Dict.py
@@ -74,3 +74,63 @@ print(mydict.keys())
 print(mydict.values())
 print(mydict.items())
 
+dictionary = {"Hello": 1234, "World": 5678} 
+print(dictionary["Hello"])
+w = dictionary.get("whatever") 
+x = dictionary.get("whatever", "nuh-uh")
+
+stock = {'eggs': 5, 'milk': 2}
+dictionary = {}
+dictionary['eggs'] = 5 
+dictionary['milk'] = 2
+mydict = {'a': [1, 2, 3], 'b': ['one', 'two', 'three']}
+mydict['a'].append(4) 
+mydict['b'].append('four')
+iterable = [('eggs', 5), ('milk', 2)] 
+dictionary = dict(iterables)
+dictionary = dict(eggs=5, milk=2)
+dictionary = dict.fromkeys((milk, eggs))
+dictionary = dict.fromkeys((milk, eggs), (2, 5))
+
+from collections import OrderedDict
+d = OrderedDict() 
+d['first'] = 1 
+d['second'] = 2
+d['third'] = 3 
+d['last'] = 4
+for key in d:   
+    print(key, d[key])
+    
+def parrot(voltage, state, action): print("This parrot wouldn't", action, end=' ')
+    print("if you put", voltage, "volts through it.", end=' ') 
+    print("E's", state, "!")
+d = {"voltage": "four million", "state": "bleedin' demised", "action": "VOOM"} >>> 
+parrot(**d)
+
+fish = {'name': "Nemo", 'hands': "fins", 'special': "gills"} 
+dog = {'name': "Clifford", 'hands': "paws", 'color': "red"} 
+fishdog = {**fish, **dog} 
+print(fishdog)
+
+dict(a=1, b=2, c=3)     
+dict([('d', 4), ('e', 5), ('f', 6)])  
+dict([('a', 1)], b=2, c=3)
+dict({'a' : 1, 'b' : 2}, c=3)  
+
+car = {}
+car["wheels"] = 4
+car["color"] = "Red"
+car["model"] = "Corvette"
+print "Little " + car["color"] + " " + car["model"] + "!"
+
+car = {"wheels": 4, "color": "Red", "model": "Corvette"}
+for key in car:  
+    print key + ": " + car[key]
+    
+
+import itertools 
+options = { "x": ["a", "b"],    "y": [10, 20, 30]}
+keys = options.keys() 
+values = (options[key] for key in keys)
+combinations = [dict(zip(keys, combination))for combination in itertools.product(*values)]
+print combinations
