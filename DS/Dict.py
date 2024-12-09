4caa914166c816d39176dd7f96744748f7b89b3a
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-12-09 17:27:27 +0100

    Create Dict.py

---
+++ DS/Dict.py
@@ -0,0 +1,11 @@
+d = {"a": 1, "b": 2, "c": 3}
+for key in d:    
+    print(key)
+for key in d.keys():    
+    print(key)
+for key in d.iterkeys():    
+    print(key)
+for value in d.values():    
+    print(value)
+for key, value in d.items():    
+    print(key, "::", value)
