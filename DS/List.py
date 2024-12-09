0b7f76c64524fae52fc782cb2d501dd272ad0d23
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-12-09 17:37:21 +0100

    Create List.py

---
+++ DS/List.py
@@ -0,0 +1,19 @@
+collection = [('a', 'b', 'c'), ('x', 'y', 'z'), ('1', '2', '3')]
+for item in collection:    
+    i1 = item[0]    
+    i2 = item[1]    
+    i3 = item[2]
+for item in collection:
+    i1, i2, i3 = item
+for i1, i2, i3 in collection:
+    
+lst = ['alpha', 'bravo', 'charlie', 'delta', 'echo']
+for s in lst:    
+    print s[:1]
+for idx, s in enumerate(lst):    
+    print("%s has an index of %d" % (s, idx))
+for i in range(2,4):   
+    print("lst at %d contains %s" % (i, lst[i]))
+for s in lst[1::2]:    
+    print(s) for i in range(1, len(lst), 2):
+        print(lst[i])
