cd9a8c29f508c832d4e37eb352398d41031b9fb8
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-12-10 05:05:34 +0100

    Update Dict.py

--- DS/Dict.py
+++ DS/Dict.py
@@ -134,3 +134,25 @@ keys = options.keys()
 values = (options[key] for key in keys)
 combinations = [dict(zip(keys, combination))for combination in itertools.product(*values)]
 print combinations
+
+{x: x * x for x in (1, 2, 3, 4)}
+dict((x, x * x) for x in (1, 2, 3, 4))
+
+{name: len(name) for name in ('Stack', 'Overflow', 'Exchange') if len(name) > 6}  
+dict((name, len(name)) for name in ('Stack', 'Overflow', 'Exchange') if len(name) > 6) 
+
+initial_dict = {'x': 1, 'y': 2} 
+{key: value for key, value in initial_dict.items() if key == 'x'}
+
+my_dict = {1: 'a', 2: 'b', 3: 'c'}
+swapped = {v: k for k, v in my_dict.items()}
+swapped = dict((v, k) for k, v in my_dict.iteritems()) 
+swapped = dict(zip(my_dict.values(), my_dict))
+swapped = dict(zip(my_dict.values(), my_dict.keys()))
+swapped = dict(map(reversed, my_dict.items()))
+print(swapped)
+
+dict1 = {'w': 1, 'x': 1} 
+dict2 = {'x': 2, 'y': 2, 'z': 2}
+{k: v for d in [dict1, dict2] for k, v in d.items()}
+{**dict1, **dict2}
