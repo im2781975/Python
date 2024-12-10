117997c37055dfbde6ef5c9272772deb6fad9941
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-12-10 06:20:05 +0100

    Update LList.py

--- DS/LList.py
+++ DS/LList.py
@@ -130,3 +130,16 @@ ll.insert(1,'e')
 ll.append('l')
 ll.append('o')
 ll.printList()
+
+class Node:
+    def __init__(self, cargo=None, next=None): 
+        self.car = cargo
+        self.cdr = next    
+    def __str__(self): 
+        return str(self.car)
+    def display(lst): 
+        if lst:         
+            w("%s " % lst)             
+            display(lst.cdr) 
+        else:            
+            w("nil\n")
