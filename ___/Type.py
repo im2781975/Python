fa1bab4c55db93ff2fc544c4f7050f85684b558c
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-12-12 14:56:44 +0100

    Create Type.py

---
+++ ___/Type.py
@@ -0,0 +1,10 @@
+#keyword
+import keyword
+print(keyword.kwlist)
+# Assign value
+a, b, _ = 1, 2, 3
+c = d = e = 1
+print(f"values are:",a, b, _, c, d, e)
+#Convert string
+a = "Hello"
+print(type(a), list[a], tuple(a), set(a))
