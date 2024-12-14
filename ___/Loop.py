3d0323477cf6768e3cd37d03e0ddb339a630c8b8
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-12-14 19:27:22 +0100

    Update Loop.py

--- ___/Loop.py
+++ ___/Loop.py
@@ -6,5 +6,20 @@ elif isinstance(i, str):
     i = int(i)
     i += 1
 print(i)
-
+#control statement
+cnt = 1
+while cnt <= 10:
+    print(cnt)
+    cnt += 1
+    if cnt == 7:
+        #continue
+        break
+    print("inner loop")
+print("outer loop")
+#range
+for i in range(1, 11):
+    if i == 7:
+        continue
+    else:
+        print(i)
 
