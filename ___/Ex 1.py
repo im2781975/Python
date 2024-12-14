b88830cbf24f0df50071190b1dd1b976afebbc89
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-12-14 21:12:25 +0100

    Create Ex 1.py

---
+++ ___/Ex 1.py
@@ -0,0 +1,21 @@
+#choose
+import random 
+diceNum = ["one", "Two", "three", "four", "five", "six"]
+num = random.randint(0, 5)
+print(diceNum[num])
+
+#Miliniel or not
+year = int(input("Enter Year:"))
+if year > 1980 and year <= 1996:
+    print("Millenial")
+elif:
+    print("Gen Z")
+
+#can vote or not
+age = int(input("Enter age:"))
+if age >= 18:
+    print("vote")
+else:
+    print("can't vote")
+
+
