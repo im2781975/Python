c4b640a40867053ba8b9a3528e1bb6df2981c05e
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-12-16 10:59:07 +0100

    Update Dict.py

--- ___/Dict.py
+++ ___/Dict.py
@@ -21,3 +21,23 @@ actress = [
 junior = filter(lambda x : x['age'] < 20, actress)
 fiver = filter(lambda x : x['age'] % 2 == 0, actress)
 print(list(junior), list(fiver))
+
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+num = set(number)
+num.add(10)
+if 5 in num: num.remove(5)
+print(num)
+for i, item in enumerate(num):
+    print(i, item)
+A, B = {1, 2, 3}, {3, 4, 5, 6, 7, 8}
+print(f"A & B: {A & B}\nA | B: {A | B} ")
+comb = {}
+comb = {(x, y)for x in A for y in B}
+print(comb)
+info = {'name' : 'A', 'Add' : 'B', 'age' : 22, 'job' : 'C'}
+print(info, info['name'])
+print(info.keys(), info.values())
+info['lang'] = 'python'
+info['name'] = 'X'
+for key, value in info.items():
+    print(key, value)
