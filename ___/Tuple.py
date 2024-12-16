359a14795896044015f03e752e2eb031949921fd
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-12-16 17:41:03 +0100

    Update Tuple.py

--- ___/Tuple.py
+++ ___/Tuple.py
@@ -18,3 +18,19 @@ print(len(letter))
 tup = ([1, 2, 3], [4, 5, 6])
 tup[0][1] = 66
 print(tup)
+
+#tuple are zero idx
+a = (1, 7, -4, "jenny", True, 9, -5)
+b, c = (10, 12, 14, 16), (11,)
+print(f"a[1]: {a[1]}\na[-2]: {a[-2]}\na[5]: {a[5]}\nlen(a): {len(a)} \n")
+print(f"a[1 : 3]: {a[1 : 3]}\na[:5]: {a[:5]} ")
+tup = (a , b, c)
+print(f"len(tup): {len(tup)}\ntup[0]: {tup[0]}\ntup: {tup}")
+tup = a + b + c
+print(f"len(tup): {len(tup)}\ntup[0]: {tup[0]}\ntup: {tup}")
+#should be same type not mixed
+print(f"max(b): {max(b)}\nmin(b): {min(b)}")
+print(f"b.count(12): {b.count(12)}\nb.index(12): {b.index(12)} ")
+lst = [2, 3, 5, 8]
+print(f"lst: {tuple(lst)}")
+print((10, ) * 6, ("Molla") * 2)
