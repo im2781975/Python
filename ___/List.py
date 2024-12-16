fbc1be06c4b19f03539b8fde68421734f16fb015
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-12-16 06:43:19 +0100

    Update List.py

--- ___/List.py
+++ ___/List.py
@@ -33,6 +33,22 @@ for item in l1:
     print("Inner")
 print("outer")
 
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+odd = []
+for n in num:
+    if n % 2 == 1:
+        odd.append(n)
+#odd = [n for n in num if n % 2 == 1 if n % 5 == 0]
+print(odd)
+
+player, age = ['A', 'B', 'C'], [21, 22, 23]
+comb = []
+for p in player:
+    for a in age:
+        comb.append([p, a])
+#comb = [(p, a)for p in player for a in age]
+print(comb)
+
 number = [5, 10, 15, 20]
 sum = 0
 for num in number:
@@ -49,3 +65,20 @@ print(number)
 num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 E = map(lambda x : x * x, num)
 print(list(E))
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(num[3], num[-3])
+#start : end
+print(f"num[:] {num[:]}\nnum[2:] {num[2:6]}\nnum[1:] {num[1:]}\nnum[:7] {num[:7]} ")
+#start : end : range
+print(f"num[1:8:1] {num[1:8:1]}\nnum[1:8:2] {num[1:8:2]}\nnum[2:8:-1] {num[2:8:-1]}\nnum[8:2:-1] {num[8:2:-1]}\nnum[8:2:-1] {num[8:2:-2]}\nnum[::-1] {num[::-1]} ")
+num.append(100)
+num.insert(6, -1)
+if 5 in num: num.remove(5)
+print(num)
+if 6 in num:
+    idx = num.index(6) 
+    print(idx)
+print(num.pop(), num)
+num.sort() 
+num.reverse()
+print(num)
