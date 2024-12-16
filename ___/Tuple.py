6186eeda589fd39d70fe3c7abb6a22883125226a
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-12-16 09:00:45 +0100

    Update Tuple.py

--- ___/Tuple.py
+++ ___/Tuple.py
@@ -10,3 +10,7 @@ print(multi())
 letter = 'A', 'B', 'C', 'D', 'E', 'F'
 print(type(letter))
 print(f"letter[0]: {letter[0]}\nletter[-1]: {letter[-1]}\nletter[5 : 2 : -1]: {letter[5 : 2 : -1]} ")
+
+tup = ([1, 2, 3], [4, 5, 6])
+tup[0][1] = 66
+print(tup)
