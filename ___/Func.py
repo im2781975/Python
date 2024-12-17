fa0de99fe62c1c9226522e61eb7205e852cbebbf
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-12-17 08:00:10 +0100

    Update Func.py

--- ___/Func.py
+++ ___/Func.py
@@ -220,3 +220,13 @@ def fun():
         yield total
 for i in fun():
     print(i, end = " ")
+
+def order(x):
+    print("Method called for value:", x)
+    return True if x > 0 else False
+"""                """
+a = order
+b = order
+c = order
+if a(-1) or b(5) or c(10):
+    print("Atleast one of the number is positive")
