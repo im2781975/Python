467f0cae4f510a152a01a09fba014fdb68d0705d
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-12-18 23:27:38 +0100

    Update Func.py

--- DS/Func.py
+++ DS/Func.py
@@ -323,3 +323,14 @@ def timer(func):
 @timer 
 def example_function():   
 example_function()
+
+"""                """
+def func(params):    
+    for value in params:        print ('Got value {}'.format(value))        
+        if value == 1:            # Returns from function as soon as value is 1            
+            print (">>>> Got 1")            
+            return        
+            
+        print ("Still looping")   
+    return "Couldn't find 1" 
+func([5, 3, 1, 2, 8, 9])
