4cb95fb624755027e2da1593cbc6d2ca844947dc
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-12-19 08:40:54 +0100

    Create Module.py

---
+++ DS/Module.py
@@ -0,0 +1,20 @@
+import random 
+print(random.randint(1, 10))
+import random as rn 
+print(rn.randint(1, 10))
+random.randrange(1, 10)
+import random
+random.randrange(1, 10)
+from math import pi 
+print(pi)   
+
+import custom
+from math import sin
+sin(1)
+
+from math import * 
+sqrt(2)    
+ceil(2.7)
+
+def sqrt(num):    print("I don't know what's the square root of {}.".format(num)) sqrt(4)
+
