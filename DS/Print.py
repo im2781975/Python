2f67598e6bd8d724ada1927bff64696e7305658f
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-12-23 18:08:44 +0100

    Create Print.py

---
+++ DS/Print.py
@@ -0,0 +1,16 @@
+foo = 1
+bar = 'bar'
+baz = 3.14
+print(str(foo) + " " + bar + " " + str(baz))
+print(foo + bar + baz)
+print("4" + "5")
+print([4] + [5])
+
+import random
+randnum = random.randint(0, 12)
+print("The randomly generated number was - " + str(randnum))
+print('apples','bananas', 'cherries', sep=', ')
+print('apple','banana', 'cherries', sep=', ')
+print("<a", end=''); print(" class='jidn'" if 1 else "", end=''); print("/>")
+print("paragraph1", end="\n\n"); print("paragraph2")
+
