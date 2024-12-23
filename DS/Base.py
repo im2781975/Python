b20afeb59d4c88cd1210149e5059d64e6a3387a5
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-12-23 17:58:00 +0100

    Update Base.py

--- DS/Base.py
+++ DS/Base.py
@@ -26,3 +26,32 @@ try:
     print(x) 
 except ZeroDivisionError:   
     print("Division by zero")
+
+from collections import Counter
+c = Counter(["a", "b", "c", "d", "a", "b", "a", "c", "d"])
+c["a"]
+Counter({"e": "e"}) 
+
+from collections import Counter
+adict = {'a': 5, 'b': 3, 'c': 5, 'd': 2, 'e':2, 'q': 5}
+Counter(adict.values()).most_common()
+Counter(adict.values()).most_common(1)
+Counter(adict.values()).most_common(2)
+
+alist = [1, 2, 3, 4, 1, 2, 1, 3, 4]
+alist.count(1)
+atuple = ('bear', 'weasel', 'bear', 'frog')
+atuple.count('bear')
+atuple.count('fox')
+
+astring = 'thisisashorttext' 
+astring.count('t')
+astring.count('th')
+astring.count('is')
+astring.count('text')
+from collections import Counter 
+Counter(astring)
+
+import numpy as np 
+a=np.array([0,3,4,3,5,4,7]) >>> 
+print np.sum(a==3)
