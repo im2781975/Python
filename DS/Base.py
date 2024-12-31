80ca65fd16117e8d4899b54ff749b96f1a65a7d3
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-12-31 16:35:36 +0600

    Update Base.py

--- DS/Base.py
+++ DS/Base.py
@@ -258,3 +258,27 @@ print(tail)
 first, second, *tail, last = [1, 2, 3]
 print(tail)
 print(last)
+
+class Vector(object):
+    def __init__(self, x, y): 
+        self.x = x 
+        self.y = y 
+    def __add__(self, v):
+        return Vector(self.x + v.x, self.y + v.y) 
+    def __sub__(self, v): 
+        return Vector(self.x - v.x, self.y - v.y) 
+    def __mul__(self, s): # Multiplication with a scalar. 
+        return Vector(self.x * s, self.y * s) 
+    def __div__(self, s):  
+        float_s = float(s) return Vector(self.x / float_s, self.y / float_s)
+    def __floordiv__(self, s):
+        return Vector(self.x // s, self.y // s)
+    def __repr__(self):
+        return '<Vector (%f, %f)>' % (self.x, self.y, ) 
+a = Vector(3, 5) 
+b = Vector(2, 7)
+print a + b
+print b - a 
+print b * 1.3 
+print a // 17 
+print a / 17 
