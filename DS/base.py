ae11343b34678da9a049b8f6a2398289b86420e2
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-12-31 09:22:36 +0600

    Update base.py

--- DS/base.py
+++ DS/base.py
@@ -180,3 +180,88 @@ if p is not None:
 else:    
     print("no get property!")
 
+def greet(name):   
+    print("Hello, {0}!".format(name))
+    print("What's your name?")
+    name = input() 
+greet(name)
+
+from itertools import chain 
+class SolarSystem:   
+    planets = [list (chain (planet, (index + 1,))) for index, planet in enumerate ((      
+        ('Mercury', 'hot', 2240),        
+        ('Venus', 'sulphurous', 6052),       
+        ('Earth', 'fertile', 6378),        
+        ('Mars', 'reddish', 3397),        
+        ('Jupiter', 'stormy', 71492),        
+        ('Saturn', 'ringed', 60268),        
+        ('Uranus', 'cold', 25559),        
+        ('Neptune', 'very cold', 24766)    ))]       
+    lines = (        '{} is a {} planet',        'The radius of {} is {} km',        '{} is planet nr. {} counting from the sun'    )       
+    def __init__ (self):        
+        self.lineIndex = 0
+    def greet (self):       
+        self.planet = self.planets [int (Math.random () * len (self.planets))]       
+        document.getElementById ('greet') .innerHTML = 'Hello {}'.format (self.planet [0])     
+        self.explain()
+    def explain (self):       
+        document.getElementById ('explain').innerHTML = (            self.lines [self.lineIndex] .format (self.planet [0], self.planet [self.lineIndex + 1]) ) 
+        self.lineIndex = (self.lineIndex + 1) % 3        
+        solarSystem = SolarSystem ()
+
+class A: 
+    def __init__ (self, x):
+        self.x = x 
+    def show (self, label):
+        print ('A.show', label, self.x)
+class B: 
+    def __init__ (self, y):        
+        alert ('In B constructor') 
+        self.y = y 
+    def show (self, label): 
+        print ('B.show', label, self.y) 
+class C (A, B): 
+    def __init__ (self, x, y):       
+        alert ('In C constructor')      
+        A.__init__ (self, x)       
+        B.__init__ (self, y) 
+        self.show ('constructor') 
+    def show (self, label):        
+        B.show (self, label) 
+        print ('C.show', label, self.x, self.y)
+a = A (1001) 
+a.show ('america') 
+b = B (2002) 
+b.show ('russia') 
+c = C (3003, 4004) 
+c.show ('netherlands') 
+show2 = c.show 
+show2 ('copy')
+
+ "£13.55".encode('ascii', errors='replace')
+"£13.55".encode('ascii', errors='ignore')
+"£13.55".encode('ascii', errors='namereplace')
+"£13.55".encode('ascii', errors='xmlcharrefreplace')
+"£13.55".encode('ascii', errors='backslashreplace')
+b = "£13.55".encode('utf8')
+b.decode('ascii', errors='replace')
+b.decode('ascii', errors='ignore')
+b.decode('ascii', errors='backslashreplace')
+type("f") == type(u"f")
+type(b"f")
+"£13.55".encode('utf8')
+"£13.55".encode('utf16')
+print type(u"£13.55".encode('utf8'))
+print u"£13.55".encode('utf8')
+b'\xc2\xa313.55'.decode('utf8')
+
+import serial
+ser = serial.Serial('/dev/ttyUSB0', 9600)
+data = ser.read()
+data = ser.read(size=5)
+data = ser.readline()
+data = ser.read(ser.inWaiting())
+ser.read(ser.inWaiting)
+
+from serial.tools import list_ports
+list_ports.comports() 
