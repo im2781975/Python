92bb71e68fe7c59260d0fd83e8dbab4f94128b98
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-12-31 12:28:32 +0600

    Update base.py

--- DS/base.py
+++ DS/base.py
@@ -265,3 +265,349 @@ ser.read(ser.inWaiting)
 
 from serial.tools import list_ports
 list_ports.comports() 
+
+ import turtle 
+ninja = turtle.Turtle()
+ninja.speed(10) 
+for i in range(180):   
+    ninja.forward(100)   
+    ninja.right(30)    
+    ninja.forward(20)   
+    ninja.left(60)    
+    ninja.forward(50)    
+    ninja.right(30)    
+    ninja.penup()    
+    ninja.setposition(0, 0)   
+    ninja.pendown()   
+    ninja.right(2) 
+turtle.done()
+
+list_object=[1,1,2,3,5,8,'a','e','i','o','u'] 
+save(list_file,list_object) 
+new_list=load(list_file) 
+new_list
+
+class Singleton(object):    
+    def __new__(cls):          
+        if not hasattr(cls, 'instance'):           
+            cls.instance = super(Singleton, cls).__new__(cls) 
+        return cls.instance
+s = Singleton() 
+print ("Object created", s) 
+s1 = Singleton() 
+print ("Object2 created", s1)
+
+from abc import ABCMeta, abstractmethod 
+class Music():    
+    __metaclass__ = ABCMeta    
+    @abstractmethod    
+    def do_play(self):        
+        pass 
+class Mp3(Music):    
+    def do_play(self):        
+        print ("Playing .mp3 music!")   
+class Ogg(Music):   
+    def do_play(self):        
+        print ("Playing .ogg music!")   
+class MusicFactory(object):    
+    def play_sound(self, object_type):        
+        return eval(object_type)().do_play()  
+if __name__ == "__main__":    
+    mf = MusicFactory()    
+    music = input("Which music you want to play Mp3 or Ogg")   
+    mf.play_sound(music)
+    
+from types import MethodType
+class Animal(object):      
+    def __init__(self, *args, **kwargs):        
+        self.name = kwargs.pop('name', None) or 'Animal'       
+        if kwargs.get('walk', None):            
+            self.walk = MethodType(kwargs.pop('walk'), self)   
+    def walk(self):        
+        """        Cause animal instance to walk               Walking functionality is a strategy, and is intended to        be implemented separately by different types of animals.        """        
+        message = '{} should implement a walk method'.format(           self.__class__.__name__)        
+        raise NotImplementedError(message)
+    def snake_walk(self):    
+        print('I am slithering side to side because I am a {}.'.format(self.name)) 
+    def four_legged_animal_walk(self):    
+        print('I am using all four of my legs to walk because I am a(n) {}.'.format(        self.name)) 
+    def two_legged_animal_walk(self):    
+        print('I am standing up on my two legs to walk because I am a {}.'.format(        self.name))
+generic_animal = Animal() 
+king_cobra = Animal(name='King Cobra', walk=snake_walk) 
+elephant = Animal(name='Elephant', walk=four_legged_animal_walk)
+kangaroo = Animal(name='Kangaroo', walk=two_legged_animal_walk)
+kangaroo.walk() 
+elephant.walk() 
+king_cobra.walk()
+generic_animal.walk()
+
+from datetime import date 
+from operator import attrgetter
+class Proxy: 
+    def __init__(self, current_user, reservation_service): 
+        self.current_user = current_user 
+        self.reservation_service = reservation_service
+    def highest_total_price_reservations(self, date_from, date_to, reservations_count): 
+        if self.current_user.can_see_reservations: 
+            return self.reservation_service.highest_total_price_reservations(                date_from,                date_to,                reservations_count ) 
+        else: 
+            return []
+            
+class Reservation: 
+    def __init__(self, date, total_price): 
+        self.date = date 
+        self.total_price = total_price
+class ReservationService: 
+    def highest_total_price_reservations(self, date_from, date_to, reservations_count): 
+        reservations = [            Reservation(date(2014, 5, 15), 100),            Reservation(date(2017, 5, 15), 10),            Reservation(date(2017, 1, 15), 50) ]
+        filtered_reservations = [r for r in reservations if (date_from <= r.date <= date_to)]        
+        sorted_reservations = sorted(filtered_reservations, key=attrgetter('total_price'), reverse=True) 
+        return sorted_reservations[0:reservations_count]
+class User:
+    def __init__(self, can_see_reservations, name): 
+        self.can_see_reservations = can_see_reservations 
+        self.name = name
+class StatsService: 
+    def __init__(self, reservation_service): 
+        self.reservation_service = reservation_service 
+    def year_top_100_reservations_average_total_price(self, year):        
+        reservations = self.reservation_service.highest_total_price_reservations(            date(year, 1, 1),            date(year, 12, 31), 1 ) 
+        if len(reservations) > 0:            
+            total = sum(r.total_price for r in reservations) 
+            return total / len(reservations)
+            else: 
+                return 0
+def test(user, year):    
+    reservations_service = Proxy(user, ReservationService())    
+    stats_service = StatsService(reservations_service)    
+    average_price = stats_service.year_top_100_reservations_average_total_price(year) 
+    print("{0} will see: {1}".format(user.name, average_price))
+    test(User(True, "John the Admin"), 2017) 
+    test(User(False, "Guest"),         2017)
+
+import hashlib 
+m = hashlib.md5()
+m.update("Nobody inspects") 
+m.update(" the spammish repetition") 
+m.digest()
+m.hexdigest()
+m.digest_size
+m.block_size
+hashlib.md5("Nobody inspects the spammish repetition").hexdigest() 'bb649c83dd1ea5c9d9dec9a18df0ffe9'
+
+h = hashlib.new('ripemd160') 
+h.update("Nobody inspects the spammish repetition") 
+h.hexdigest()
+
+a = 1 
+id(a)
+a += 2
+
+stack = "Overflow"
+id(stack)
+stack += " rocks!"
+id(stack)
+ 
+stack = "Stack" 
+stackoverflow = stack + "Overflow" 
+id(stack)
+id(stackoverflow)
+
+s = ""
+for i in range(1, 1000):    
+    s += str(i)    
+    s += ","
+
+b = bytearray(b'Stack')
+id(b)
+b += b'Overflow'
+bytearray(b'StackOverflow') 
+id(b)
+
+c = b 
+c += b' rocks!' 
+bytearray(b'StackOverflow rocks!')
+id(c) == id(b)
+
+ll = [ [] ]*4
+ll[0].append(23)
+
+def list_add3(lin):    
+    lin += [3]    
+    return lin
+a = [1, 2, 3]
+b = list_add3(a)
+
+def tuple_add3(tin):    
+    tin += (3,)    
+    return tin
+a = (1, 2, 3) 
+b = tuple_add3(a) 
+
+def yoda(prologue, sentence):    
+    sentence.reverse()    
+    prologue += " ".join(sentence)    
+    return prologue
+focused = ["You must", "stay focused"] 
+saying = "Yoda said: " 
+yoda_sentence = yoda(saying, focused)
+
+orders_df = pd.DataFrame() 
+orders_df['customer_id'] = [1,1,1,1,1,2,2,3,3,3,3,3] 
+orders_df['order_id'] = [1,1,1,2,2,3,3,4,5,6,6,6] 
+orders_df['item'] = ['apples', 'chocolate', 'chocolate', 'coffee', 'coffee', 'apples', 'bananas', 'coffee', 'milkshake', 'chocolate', 'strawberry', 'strawberry']
+print(orders_df)
+
+count_number_of_orders = lambda x: len(x.unique())
+orders_df['number_of_orders_per_cient'] = (orders_df .groupby(['customer_id'])['order_id']  .transform(count_number_of_orders))
+print(orders_df)
+
+def multiple_items_per_order(_items): 
+    multiple_item_bool = _items.duplicated(keep=False) 
+    return(multiple_item_bool)
+    
+orders_df['item_duplicated_per_order'] = (orders_df .groupby(['order_id'])['item'] .transform(multiple_items_per_order))
+print(orders_df)
+
+from ply import lex import ply.yacc as yacc
+tokens = (    'PLUS',    'MINUS',    'TIMES',    'DIV',    'LPAREN',    'RPAREN',    'NUMBER', )
+
+t_ignore = ' \t'
+t_PLUS    = r'\+'
+t_MINUS   = r'-'
+t_TIMES   = r'\*' 
+t_DIV     = r'/' 
+t_LPAREN  = r'\(' 
+t_RPAREN  = r'\)'
+def t_NUMBER( t ) :   
+    r'[0-9]+'    
+    t.value = int( t.value )    
+    return t
+def t_newline( t ):
+    r'\n+'  
+    t.lexer.lineno += len( t.value )
+def t_error( t ):
+    print("Invalid Token:",t.value[0]) 
+    t.lexer.skip( 1 )
+lexer = lex.lex()
+
+precedence = ( ( 'left', 'PLUS', 'MINUS' ), ( 'left', 'TIMES', 'DIV' ), ( 'nonassoc', 'UMINUS' ) )
+def p_add( p ) : 
+    'expr : expr PLUS expr'   
+    p[0] = p[1] + p[3]
+def p_sub( p ) :
+    'expr : expr MINUS expr'   
+    p[0] = p[1] - p[3]
+def p_expr2uminus( p ) :
+    'expr : MINUS expr %prec UMINUS'  
+    p[0] = - p[2]
+def p_mult_div( p ) : '''expr : expr TIMES expr            | expr DIV expr''' 
+    if p[2] == '*' :        
+        p[0] = p[1] * p[3]
+    else :
+        if p[3] == 0 :
+            print("Can't divide by 0") 
+            raise ZeroDivisionError('integer division by 0')       
+        p[0] = p[1] / p[3]
+def p_expr2NUM( p ) : 
+    'expr : NUMBER'   
+    p[0] = p[1]
+def p_parens( p ) : 
+    'expr : LPAREN expr RPAREN' 
+    p[0] = p[2]
+def p_error( p ): 
+    print("Syntax error in input!")
+parser = yacc.yacc()
+res = parser.parse("-4*-(3-5)")
+print(res)
+
+import ply.lex as lex
+tokens = [ 'NUMBER', 'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'LPAREN', 'RPAREN', ]
+
+t_ignore = ' \t'
+t_PLUS    = r'\+'
+t_MINUS   = r'-'
+t_TIMES   = r'\*' 
+t_DIV     = r'/' 
+t_LPAREN  = r'\(' 
+t_RPAREN  = r'\)'
+def t_NUMBER( t ) :   
+    r'[0-9]+'    
+    t.value = int( t.value )    
+    return t
+def t_newline( t ):
+    r'\n+'  
+    t.lexer.lineno += len( t.value )
+def t_error( t ):
+    print("Invalid Token:",t.value[0]) 
+    t.lexer.skip( 1 )
+lexer = lex.lex()
+lexer.input(data)
+while True:        
+    tok = lexer.token()
+    if not tok: 
+        break      
+    print(tok)
+    
+import ply.lex as lex  
+class MyLexer(object):                    
+    def build(self, **kwargs):           
+        self.lexer = lex.lex(module=self, **kwargs)       
+    def test(self, data):           
+        self.lexer.input(data)          . 
+        for token in self.lexer.token():              
+            print(token)
+m = MyLexer() 
+m.build()          
+m.test("3 + 4")
+
+import ply.yacc as yacc
+from calclex import tokens
+def p_expression_plus(p):
+    p[0] = p[1] + p[3]
+def p_expression_minus(p):
+    p[0] = p[1] - p[3]
+def p_expression_term(p):
+    p[0] = p[1]
+def p_term_times(p):
+    p[0] = p[1] * p[3]
+def p_term_div(p): 
+    p[0] = p[1] / p[3]
+def p_term_factor(p): 
+    p[0] = p[1]
+def p_factor_num(p): 
+    p[0] = p[1]
+def p_factor_expr(p): 
+    p[0] = p[2]
+def p_error(p):
+    print("Syntax error in input!")
+parser = yacc.yacc()
+while True: 
+    try:      
+        s = raw_input('calc > ') 
+    except EOFError: 
+        break
+    if not s: continue   
+    result = parser.parse(s)
+    print(result)
+    
+def p_binary_operators(p):
+    if p[2] == '+':         
+        p[0] = p[1] + p[3]      
+    elif p[2] == '-':         
+        p[0] = p[1] - p[3]      
+    elif p[2] == '*':         
+        p[0] = p[1] * p[3]     
+    elif p[2] == '/':         
+        p[0] = p[1] / p[3]
+def p_binary_operators(p):
+    if p[2] == '+':          
+        p[0] = p[1] + p[3]      
+    elif p[2] == '-':          
+        p[0] = p[1] - p[3]      
+    elif p[2] == '*':         
+        p[0] = p[1] * p[3]     
+    elif p[2] == '/':          
+        p[0] = p[1] / p[3]
+        
