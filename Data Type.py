6fbc443bf0a54f06d66109f06b094fd03aca8821
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-01-02 20:52:46 +0600

    Update Data Type.py

--- Data Type.py
+++ Data Type.py
@@ -64,3 +64,31 @@ mb = Car(s=190, c='white')
 # printing the color and speed of cars
 print(audi.color)
 print(bmw.speed)
+
+class GeeksforGeeks:
+    def __init__(self, topic):
+        self.topic = topic
+ 
+    def display_topic(self):
+        print("Topic:", self.topic)
+ 
+# Creating an instance of GeeksforGeeks
+gfg_instance = GeeksforGeeks("Python")
+ 
+# Calling the display_topic method
+gfg_instance.display_topic()
+/////
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+ 
+    def calculate_area(self):
+        area = 3.14 * self.radius ** 2
+        return area
+ 
+# Creating an instance of Circle
+circle_instance = Circle(5)
+ 
+# Calling the calculate_area method
+print("Area of the circle:", circle_instance.calculate_area())
+////
