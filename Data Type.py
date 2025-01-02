bea785f216a14d2bc83870491c0dee36c8cc229e
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-01-02 17:07:06 +0600

    Update Data Type.py

--- Data Type.py
+++ Data Type.py
@@ -29,3 +29,38 @@ class Colour(Enum):
     green = 2   
     blue = 3
 [c for c in Colour]
+
+class Car():
+    # args receives unlimited no. of arguments as an array
+    def __init__(self, *args):
+        # access args index like array does
+        self.speed = args[0]
+        self.color = args[1]
+
+
+# creating objects of car class
+audi = Car(200, 'red')
+bmw = Car(250, 'black')
+mb = Car(190, 'white')
+
+# printing the color and speed of the cars
+print(audi.color)
+print(bmw.speed)
+///
+# defining car class
+class Car():
+    # args receives unlimited no. of arguments as an array
+    def __init__(self, **kwargs):
+        # access args index like array does
+        self.speed = kwargs['s']
+        self.color = kwargs['c']
+
+
+# creating objects of car class
+audi = Car(s=200, c='red')
+bmw = Car(s=250, c='black')
+mb = Car(s=190, c='white')
+
+# printing the color and speed of cars
+print(audi.color)
+print(bmw.speed)
