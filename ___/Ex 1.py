bdee0da6e9057c5834809ddf6da9a7820a95dd73
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-01-04 19:29:52 +0600

    Update Ex 1.py

--- ___/Ex 1.py
+++ ___/Ex 1.py
@@ -205,3 +205,17 @@ symbols = punctuation + ascii_letters + digits
 secure = random.SystemRandom()
 password = "".join(secure.choice(symbols)for i in range(10))
 print(password)
+"""				"""
+import string
+from random import SystemRandom
+secure = SystemRandom()
+alphabet = string.ascii_letters + string.digits
+while True:
+    password = ''.join(secure.choice(alphabet) for i in range(10))
+    if (any(c.islower() for c in password)
+        and any(c.isupper() for c in password)
+        and sum(c.isdigit() for c in password) >= 3):
+        break
+print("Generated secure password:", password)
+print("List of 10 random digits:", [secure.randrange(10) for i in range(10)])
+print("Random integer between 0 and 20:", secure.randint(0, 20))
