05456315a2c988dff06550f60ff6328f899cfb88
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-01-04 17:24:20 +0600

    Update Set.py

--- ___/Set.py
+++ ___/Set.py
@@ -6,9 +6,18 @@ tes.add('z')
 print(tes, st)
 frset = frozenset("Mollvai")
 print(frset)
-
+"""						"""
 strset = set("mollavai")
 lstset = set([1, 2, "am", "ironman", 6, 9])
 print(f"strset: {strset}\nlstset: {lstset}\n")
 for i in lstset:
     print(i, end = " ")
+"""						"""
+st = {1, 2, 3}
+i = iter(st)
+print(i, next(i), next(i))
+for a in st:
+    print(a, end = " ")
+lst = list(st)
+tsl = [a * 2 for a in st if a > 2]  
+print(tsl)
