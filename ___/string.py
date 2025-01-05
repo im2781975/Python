95430abd636ab58ac5d9c76276810abeaa01bf53
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-01-05 17:10:19 +0600

    Update string.py

--- ___/string.py
+++ ___/string.py
@@ -165,4 +165,180 @@ print("I am from {0}. I love cupcakes from {0}!".format("Australia"))
 print("{'a': 5, 'b': 6}")
 print("{{'{}': {}, '{}': {}}}".format("a", 5, "b", 6))
 print(f"{{'{'a'}': {5}, '{'b'}': {6}}} ")
+"""				"""
+import string
+print(string.ascii_letters, 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+print(string.ascii_lowercase, 'abcdefghijklmnopqrstuvwxyz')
+print(string.ascii_uppercase,  'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+print(string.digits, '0123456789')
+print(string.hexdigits, '0123456789abcdefABCDEF')
+print(string.octdigits, '01234567')
+print(string.punctuation, r'!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~')
+print(string.whitespace, '\t\n\r\x0b\x0c')
+print(string.printable,  '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c')
+print(" a line with leading and trailing space ".strip(), 'a line with leading and trailing space')
+print(">>> a Python prompt".strip('> '))
+print("spacious string      ".rstrip(), '     spacious string')
+print("     spacious string      ".rstrip(), 'spacious string      ')
+print(reversed("Hello"))
+print([char for char in reversed('hello')])
+print(''.join(reversed('hello')))
+print("This is a sentence.".split())
+print(" This is    a sentence.  ".split())
+print("            ".split())
+print("This is a sentence.".split(' '))
+print("Earth,Stars,Sun,Moon".split(','))
+print(" This is    a sentence.  ".split(' '))
+print("This is a sentence.".split('e'))
+print("This is a sentence.".split('en'))
+print("This is a sentence.".split('e', maxsplit = 0))
+print("This is a sentence.".split('e', maxsplit = 1))
+print("This is a sentence.".split('e', maxsplit = 2))
+print("This is a sentence.".split('e', maxsplit =- 1))
+print("This is a sentence.".rsplit('e', maxsplit = 1))
+print("This is a sentence.".rsplit('e', maxsplit = 2))
+print("Make sure to foo your sentence.".replace('foo', 'spam'))
+print("It can foo multiple examples of foo if you want.".replace('foo', 'spam'))
+print("""It can foo multiple examples of foo if you want,  ... or you can limit the foo with the third argument.""".replace('foo', 'spam', 1))
+print("Hello World".isalpha())
+print("Hello2World".isalpha())
+print("HelloWorld!".isalpha())
+print("HelloWorld".isalpha())
+print("HeLLO WORLD".isupper())
+print("HELLO WORLD".isupper())
+print("".isupper())
+print("Hello world".islower())
+print("hello world".islower())
+print("".islower())
+print("hello world".istitle())
+print("Hello world".istitle())
+print("Hello World".istitle())
+print("".istitle())
+print("Hello2World".isalnum())
+print("HelloWorld".isalnum())
+print("2016".isalnum())
+print("Hello World".isalnum())
+print("\t\r\n".isspace())
+print(" ".isspace())
+print("".isspace())
+print("foo" in "foo.baz.bar")
+print("" in "test")
+print(" ".join(["once","upon","a","time"]))
+print("---".join(["once", "upon", "a", "time"]))
+print("ß".lower(), "ß".upper().lower(), "ß".upper().lower())
+"""            """
+def reverse(ing):
+    return ing[::-1]
+print(reverse('Hello'))
+ing = "Values are a: {a}, c: {c}, e: {e}"
+print(ing.format(a = "1"*1, c = "3"*3, e = "5"*5))
+"""            """
+ing = ''
+print(ing.isspace())
+print(ing.isspace() or not ing)
+print(not ing.strip())
+"""            """
+ing = "She sells seashells by the seashore."
+start = 10
+print(ing.count("sh"), ing.count("sea", start))
+tr = ing[start:]
+print(tr.count("sea"))
+"""            """
+lengths = {5: (1381, 2222), 19: (63, 102),    40: (2555, 4112), 93: (189,305),}
+for road, length in lengths.items():    miles,kms = length
+print('{} -> {} mi. ({} km.)'.format(str(road).rjust(4), str(miles).ljust(4), str(kms).ljust(4)))
+"""            """
+ing = "This is a test string"
+print(ing.startswith("T"), ing.startswith("is", 2), ing.startswith(('This', 'That')))
+print(ing.startswith(('ab', 'bc')), ing.endswith('.'), ing.endswith('!'), ing.endswith('stop.'))
+print(ing.endswith('Stop.'), ing.endswith(('.', 'something')), ing.endswith(('ab', 'bc')))
+"""            """
+ing = b'\xc2\xa9 abc'
+print(ing[0], type(ing))
+u = ing.decode('utf-8')
+print(u[0], type(u), u.encode('utf-8'))
+"""            """
+import re
+pattern, ing = r"123", "123zzb"
+re.match(pattern, ing)
+match = re.match(pattern, ing)
+print(match.group())
 
+pattern, ing = r"\t123", "\t123zzb" 
+print(re.match(pattern, ing).group())
+print(re.match(pattern, "\t123zzb").group())  
+
+pattern = r"\t123" 
+print(re.match(pattern, ing).group())
+match = re.match(r"(123)", "a123zzb")
+print(match is None)
+match = re.search(r"(123)", "a123zzb")
+print(match.group())
+
+pattern = r"(your base)"
+sentence = "All your base are belong to us."
+match = re.search(pattern, sentence)
+print(match.group(1))
+match = re.search(r"(belong.*)", sentence)
+print(match.group(1))
+
+match = re.search(r"^123", "123zzb")
+print(match.group(0))
+match = re.search(r"^123", "a123zzb")
+print(match is None)
+match = re.search(r"123$", "zzb123")
+print(match.group(0))
+
+print(re.search("b", "ABC") is None)
+print(re.search("b", "ABC", flags=re.IGNORECASE).group())
+print(re.search("a.b", "A\nBC", flags=re.IGNORECASE) is None)
+m = re.search("a.b", "A\nBC", flags=re.IGNORECASE|re.DOTALL)
+print(m.group())
+print(re.sub(r"t[0-9][0-9]", "foo", "my name t13 is t44 what t99 ever t44"))
+print(re.sub(r"t([0-9])([0-9])", r"t\2\1", "t13 t19 t81 t25"))
+print(re.sub(r"t([0-9])([0-9])", r"t\g<2>\g<1>", "t13 t19 t81 t25"))
+items = ["zero", "one", "two"]
+print(re.sub(r"a\[([0-3])\]", lambda match: items[int(match.group(1))], "Items: a[0], a[1], something, a[2]"))
+print(re.findall(r"[0-9]{2,3}", "some 1 text 12 is 945 here 4445588899"))
+results = re.finditer(r"([0-9]{2,3})", "some 1 text 12 is 945 here 4445588899")
+print(results)
+for result in results:     
+    print(result.group(0), end = " ")
+"""            """
+def Isallowed(string):    
+    characherRegex = re.compile(r'[^a-zA-Z0-9.]')    
+    string = characherRegex.search(string)  
+    return not bool(string)
+print (Isallowed("abyzABYZ0099"))
+print (Isallowed("#*@#$%^"))
+print(re.split(r'\s+', 'James 94 Samantha 417 Scarlett 74'))
+"""            """
+
+import re
+sentence, pattern = "This is a phone number 672-123-456-9910" , r".*(phone).*?([\d-]+)"
+match = re.match(pattern, sentence)
+print(match.group(), match.group(0), match.group(1), match.group(1, 2))
+"""            """
+match = re.search(r'My name is (?P<name>[A-Za-z ]+)', 'My name is John Smith')
+print(match.group('name'), match.group(1))
+print(re.match(r'(\d+)(\+(\d+))?', '11+22').groups())
+print(re.match(r'(\d+)(?:\+(\d+))?', '11+22').groups())
+match = re.search(r'[b]', 'a[b]c') 
+print(match.group())
+match = re.search(r'\[b\]', 'a[b]c') 
+print(match.group())
+re.escape('a[b]c')
+match = re.search(re.escape('a[b]c'), 'a[b]c') 
+print(match.group())
+"""            """
+username = 'A.C.'
+print(re.findall(r'Hi {}!'.format(username), 'Hi A.C.! Hi ABCD!'))
+print(re.findall(r'Hi {}!'.format(re.escape(username)), 'Hi A.C.! Hi ABCD!'))
+"""            """
+text = 'You can try to find an ant in this string' 
+pattern = 'an?\\w' 
+for match in re.finditer(pattern, text):
+    sStart = match.start()
+    sEnd = match.end()
+    sGroup = match.group()
+    print('Match "{}" found at: [{},{}]'.format(sGroup, sStart,sEnd))
