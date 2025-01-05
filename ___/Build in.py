4592c7637dadaea402021f1da43860fad8380dd1
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-01-05 20:59:41 +0600

    Update Build in.py

--- ___/Build in.py
+++ ___/Build in.py
@@ -264,11 +264,13 @@ from functools import reduce
 def factorial(n):
     return reduce(lambda a, b: (a * b), range(1, n + 1)
 """				"""
+import dis
 def fib(n):
     if n <= 2:
         return 1
     return fib(n - 1) + fib(n - 2)
 print(dir(fib.__code__))
+print(dis.dis(fib)
 import random
 import inspect
 print(inspect.getsource(random.randrange))
