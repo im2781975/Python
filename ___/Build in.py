e07d996fb40aa683729243b6f514659067f5b615
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-01-05 19:05:35 +0600

    Update Build in.py

--- ___/Build in.py
+++ ___/Build in.py
@@ -128,3 +128,12 @@ if random.random() < probability:
     print("Decision with probability 0.3") 
 else:    
     print("Decision with probability 0.7")
+    
+from Queue import Queue 
+question_queue = Queue() 
+for x in range(1,10):   
+    temp_dict = ('key', x)    
+    question_queue.put(temp_dict) 
+while(not question_queue.empty()):    
+    item = question_queue.get()    
+    print(str(item))
