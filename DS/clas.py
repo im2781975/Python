1385432314c51200a3e9a1df16224a49f3d368d5
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-01-06 17:56:59 +0600

    Update clas.py

--- DS/clas.py
+++ DS/clas.py
@@ -937,3 +937,57 @@ class Car(object):
     def __init__(self):        
         self.color = "red"        
         self.wheels = [Wheel(), Wheel(), Wheel(), Wheel()]
+"""				"""
+class IntegerContainer(object): 
+    def __init__(self, value): 
+        self.value = value __ne__ and __eq__:
+    def __repr__(self): 
+        return "{}({})".format(self.__class__.__name__, self.value)
+    def __lt__(self, other): 
+        print('{!r} - Test less than {!r}'.format(self, other)) 
+        return self.value < other.value 
+    def __le__(self, other): 
+        print('{!r} - Test less than or equal to {!r}'.format(self, other))
+        return self.value <= other.value
+    def __gt__(self, other): 
+        print('{!r} - Test greater than {!r}'.format(self, other)) 
+        return self.value > other.value 
+    def __ge__(self, other): 
+        print('{!r} - Test greater than or equal to {!r}'.format(self, other)) 
+        return self.value >= other.value 
+    def __eq__(self, other):
+        print('{!r} - Test equal to {!r}'.format(self, other)) 
+        return self.value == other.value
+    def __ne__(self, other):
+        print('{!r} - Test not equal to {!r}'.format(self, other)) 
+        return self.value != other.value
+alist = [IntegerContainer(5), IntegerContainer(3),         IntegerContainer(10), IntegerContainer(7) ]
+res = max(alist)
+print(res)
+res = min(alist)  
+print(res)
+res = sorted(alist)
+print(res)
+res = sorted(alist, reverse=True)
+print(res)
+del IntegerContainer.__lt__ 
+res = min(alist)
+print(res)
+
+import functools 
+@functools.total_ordering 
+class IntegerContainer(object):    
+    def __init__(self, value):        
+        self.value = value           
+    def __repr__(self):        
+        return "{}({})".format(self.__class__.__name__, self.value)
+    def __lt__(self, other):        
+        print('{!r} - Test less than {!r}'.format(self, other))        
+        return self.value < other.value       
+    def __eq__(self, other):       print('{!r} - Test equal to {!r}'.format(self, other))        
+        return self.value == other.value       
+    def __ne__(self, other):        
+        print('{!r} - Test not equal to {!r}'.format(self, other))        
+        return self.value != other.value
+IntegerContainer(5) > IntegerContainer(6)
+IntegerContainer(6) > IntegerContainer(5)
