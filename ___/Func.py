03c48ff60bbb012e7393543ddcb05463bb5bde32
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-01-08 07:40:52 +0600

    Update Func.py

--- ___/Func.py
+++ ___/Func.py
@@ -892,3 +892,253 @@ omnivores = ['chicken', 'dove', 'mouse', 'pig']
 def animals(w, x, y, z):    
     return '{0}, {1}, {2}, and {3} ARE ALL ANIMALS'.format(w.title(), x, y, z)
 import pprint pprint.pprint(list(map(animals, insects, carnivores, herbivores, omnivores)))
+"""				"""
+class NumberNotInRangeException(Exception):
+    pass
+def raise_power(x, y): 
+    if y in (3, 4, 5): 
+        return x ** y 
+    raise NumberNotInRangeException("You should provide a valid exponent (3, 4, or 5)")
+from functools import partial  
+raise_to_three = partial(raise_power, y = 3)
+raise_to_four = partial(raise_power, y = 4)
+raise_to_five = partial(raise_power, y = 5)
+print(raise_to_three(2))  
+print(raise_to_four(2))   
+print(raise_to_five(2))   
+
+def func(params):    
+    for value in params:  
+        print('Got value {}'.format(value))   
+        if value == 1:  
+            print(">>>> Got 1")  
+            return  
+        print("Still looping")   
+    return "Couldn't find 1"  
+result = func([5, 3, 1, 2, 8, 9])
+print(result)
+def function():
+    for x in range(10):
+        yield x ** 2
+print(sum(i for i in range(10) if i % 2 == 0))
+foo = [1, 2, 3, 4]
+print(any(x == 0 for x in foo))
+print(type(a > b for a in foo if a % 2 == 1))
+print(sum((i for i in range(10) if i % 2 == 0)))
+print(any((x == 0 for x in foo)))
+result_type_gen = type((a > b for a in foo if a % 2 == 1))
+print("Type of generator expression (parens):", result_type_gen)
+from functools import reduce
+lem = lambda x : x * x
+print(lem(2))
+print(map(len, ["Mary", "Isla", "Sam"]))
+print(reduce(lambda a, x: a + x, [0, 1, 2, 3, 4]))
+arr = [1, 2, 3, 4, 5, 6]
+print([i for i in filter(lambda x : x > 4,arr)])
+import operator
+print(reduce(operator.mul, [10, 5, -3]))
+print(reduce(operator.and_, [False, True, True, True]))
+print(reduce(operator.or_, [True, False, False, False]))
+def raise_power(x, y): 
+    if y in (3, 4, 5): 
+        return x ** y 
+    raise ValueError("You should provide a valid exponent (3, 4, or 5)")
+from functools import partial  
+raise_to_three = partial(raise_power, y=3)
+raise_to_four = partial(raise_power, y=4)
+raise_to_five = partial(raise_power, y=5)
+print(raise_to_three(2))  
+print(raise_to_four(2))   
+print(raise_to_five(2))   
+try:
+    print(raise_power(2, 6))
+except ValueError as e:
+    print(f"Error: {e}")
+
+def print_args(func):    
+    def inner_func(*args, **kwargs):    
+        print("Arguments:", args)  
+        print("Keyword arguments:", kwargs)  
+        return func(*args, **kwargs)
+    return inner_func
+@print_args
+def multiply(num_a, num_b):    
+    return num_a * num_b 
+print("Result of multiplication:", multiply(3, 5))
+def func(params):    
+    for value in params:        
+        print ('Got value {}'.format(value))        
+        if value == 1:            
+            print (">>>> Got 1")      
+            return        
+        print ("Still looping")   
+    return "Couldn't find 1" 
+print(func([5, 3, 1, 2, 8, 9]))
+def function():
+    for x in range(10):
+        yield x ** 2
+print(sum(i for i in range(10) if i % 2 == 0))
+foo = [1, 2, 3, 4]
+print(any(x == 0 for x in foo))
+print(type(a > b for a in foo if a % 2 == 1))
+print(sum((i for i in range(10) if i % 2 == 0)))
+print(any((x == 0 for x in foo)))
+print(type((a > b for a in foo if a % 2 == 1)))
+print(sum(i ** 2 for i in range(4)))
+import itertools
+def starting_from(n):    
+    while True:        
+        yield n        
+        n += 1
+natural = starting_from(1)
+natural = itertools.count(1)
+multiples_of_two = (x * 2 for x in natural) 
+multiples_of_three = (x for x in natural if x % 3 == 0)
+list(multiples_of_two)
+first_five_multiples_of_three = [next(multiples_of_three) for _ in range(5)]
+from itertools import islice 
+multiples_of_four = (x * 4 for x in starting_from(1)) 
+first_five_multiples_of_four = list(islice(multiples_of_four, 5))
+next(natural)    
+next(multiples_of_two)   
+next(multiples_of_four) 
+for idx, number in enumerate(multiplies_of_two): 
+    print(number)    
+    if idx == 9:
+        break
+
+import itertools
+def fibonacci():    
+    a, b = 1, 1   
+    while True:        
+        yield a        
+        a, b = b, a + b
+first_ten_fibs = list(itertools.islice(fibonacci(), 10))
+def nth_fib(n):    
+    return next(itertools.islice(fibonacci(), n - 1, n))
+ninety_nineth_fib = nth_fib(99)
+
+def accumulator():    
+    total = 0    
+    value = None    
+    while True:      
+        value = yield total      
+        if value is None: 
+            break       
+        total += value
+generator = accumulator()
+next(generator)    
+generator.send(1)    
+generator.send(10)   
+generator.send(100) 
+next(generator)
+
+def foob(x):    
+    yield from range(x * 2)    
+    yield from range(2)
+print(list(foob(5)))
+def fibto(n):   
+    a, b = 1, 1    
+    while True:      
+        if a >= n: break        
+        yield a        
+        a, b = b, a + b
+def usefib():  
+    yield from fibto(10)  
+    yield from fibto(20)
+print(list(usefib()))
+def xrange(n):   
+    i = 0   
+    while i < n:       
+        yield i       
+        i += 1
+for i in xrange(10):   
+    print(i)
+a, b, c = xrange(3)
+print(list(xrange(10)))
+def nums():    
+    yield 1
+    yield 2    
+    yield 3 
+generator = nums()
+next(generator, None)  
+next(generator, None)  
+next(generator, None)  
+next(generator, None)  
+def coroutine(func):    
+    def start(*args,**kwargs):        
+        cr = func(*args,**kwargs)        
+        next(cr)        
+        return cr    
+    return start
+@coroutine
+def adder(sum = 0):   
+    while True:       
+        x = yield sum   
+        sum += x
+s = adder() 
+s.send(1) 
+s.send(2) 
+def create_gen():      
+    yield value    
+    return
+print(list(create_gen()))
+def preorder_traversal(node):    
+    yield node.value    
+    for child in node.children:        
+        yield from preorder_traversal(child)
+generator = (i * 2 for i in range(3))
+next(generator)  
+next(generator)  
+next(generator)  
+next(generator)  
+def fib(a = 0, b = 1):   
+    while True:      
+        yield a        
+        a, b = b, a + b 
+f = fib() 
+print(', '.join(str(next(f)) for _ in range(10)))
+
+def find_and_transform(sequence, predicate, func):  
+    for element in sequence:        
+        if predicate(element):  
+            return func(element)
+    raise ValueError("No element satisfies the predicate")  
+my_sequence = [1, 2, 3, 4, 5]
+my_predicate = lambda x: x % 2 == 0
+my_func = lambda x: x * x  
+item = find_and_transform(my_sequence, my_predicate, my_func)
+print("Transformed item:", item)
+item = next(my_func(x) for x in my_sequence if my_predicate(x))
+print("Transformed item using generator:", item)
+def first(generator): 
+    try: 
+        return next(generator)  
+    except StopIteration:
+        raise ValueError("Generator is empty")  
+gen = (x for x in my_sequence if x % 2 == 0)  
+print("First even number:", first(gen))
+a = [1, 2, 3]
+b = ['a', 'b', 'c']
+for x, y in zip(a, b): 
+    print(x, y)  
+
+from functools import reduce
+import operator
+def add(s1, s2):  
+    return s1 + s2
+asequence = [1, 2, 3, 4, 5]
+sum_result = reduce(add, asequence)
+print("Sum of elements:", sum_result)
+sum_result = reduce(operator.add, asequence)
+print("Sum using operator.add:", sum_result)
+sum_result_with_initial = reduce(add, asequence, 10)  
+print("Sum with initial value 10:", sum_result_with_initial)
+def multiply(s1, s2):    
+    print(f'{s1} * {s2} = {s1 * s2}')  
+    return s1 * s2
+asequence = [1, 2, 3]
+cumprod_with_initial = reduce(multiply, asequence, 5)
+print("Cumulative product with initial value 5:", cumprod_with_initial)
+cumprod_without_initial = reduce(multiply, asequence)
+print("Cumulative product without initial value:", cumprod_without_initial)
