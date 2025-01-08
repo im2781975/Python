4bbf5b945c9ddf70cc7781074c6e47dd954b3a02
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-01-08 07:37:55 +0600

    Update Func.py

--- ___/Func.py
+++ ___/Func.py
@@ -624,3 +624,271 @@ def fib(i, current = 0, next = 1):
     else:    
         return fib(i - 1, next, current + next)
 print(fib(10))  
+"""				"""
+def input_num(msg, errormsg=None):
+    while True:        
+        try:            
+            return float(input(msg))       
+        except ValueError:      
+            if errormsg is not None: 
+                print(errormsg)
+            else:
+                print("Invalid Input")
+#num = input_num("Enter: "); print(num)
+try:   
+    x = 1.0 / 0.0    
+    print(x) 
+except ZeroDivisionError:   
+    print("Division by zero")
+print("Hello ", end = "<br>")
+print("Hello", end = "Break")
+def twoSum(a, b):
+    return a + b
+def twoSum(a : int, b : int) ->int:
+    return a + b
+def twoSum(a : str, b : str):
+    return a + b
+print(twoSum("a", "b"))
+def func1(arg1, arg2, arg3):
+    return (arg1, arg2, arg3)
+print(func1(1, 2, 3), func1(*[6, 7], 9))
+dic = {'arg1': 1, 'arg2': 2,'arg3': 3 }
+print(func1(**dic))
+def func2(arg1 = 'a', arg2 = 'b', arg3 = 'c'):
+    return (arg1, arg2, arg3)
+print(func2(1), func2(2, 3), func2(arg2 = 2, arg3 = 3))
+dic = {'arg1': 1, 'arg2': 2, 'arg3': 3 } 
+print(func2(**dic), func2(**{'arg2': 2}))
+def func3(arg1, arg2='b', arg3='c'):
+    return (arg1, arg2, arg3)
+print(func3(*[1]))  
+print(func3(*[1, 2, 3]))  
+print(func3(**{'arg1' : 1}))  
+print(func3(**{'arg1' : 1, 'arg2' : 2, 'arg3' : 3}))  
+print(func3(*[1, 2], **{'arg3' : 3}))  
+def func(*args, **kwargs):    
+    print(args, kwargs)
+print(func(1, 2, 3), func(a = 1, b = 2, c = 3), func(1, 2, 3), func('x', 'y', 'z', a = 1, b = 2, c = 3))
+
+
+#Find Divisors
+div = int(input("Enter integers: "))
+for x in range(1, div + 1):
+    if div / x == div // x:  
+        print(x, end = " ")
+print()
+for i in range(4):   
+    d = i * 2
+    print(f"d at iteration {i}: {d}")
+print("Final value of d:", d) 
+"""            """
+def switch(value):   
+    if value == 1:
+        return "one"   
+    if value == 2:       
+        return "two"    
+    if value == 42:       
+        return "Error"    
+    raise Exception("No case found!")
+switch(1)
+switch = {1: lambda: 'one', 2: lambda: 'two', 42: lambda: 'Error',}
+def default_case():    
+    raise Exception('No case found!')
+switch.get(1, default_case)()
+
+def run_switch(value):    
+    return switch.get(value, default_case)()
+run_switch(1)
+def greet_two(greeting):
+    print(greeting)
+greet_two("Howdy")
+def greet_two(greeting = "Howdy"):    
+    print(greeting)
+greet_two()
+def many_types(x):    
+    if x < 0:        
+        return "Hello!"    
+    else:        
+        return 0 
+print(many_types(1)) 
+print(many_types(-1))
+def func(*args):
+    for i in args:        
+        print(i) 
+func(1, 2, 3)
+func(*[1, 2, 3])
+def func(**kwargs):   
+    for name, value in kwargs.items():  
+        print(name, value)
+func(x = 1, y = 2, z = 3)
+dic = {'foo': 1, 'bar': 2}
+func(**dic)       
+def fn(**kwargs):   
+    print(kwargs)   
+    f1(**kwargs)
+def f1(**kwargs):   
+    print(len(kwargs))
+fn(a = 1, b = 2)
+
+def greeting():    
+    return "Hello"
+print(greeting())
+greet_me = lambda: "Hello"
+print(greet_me())
+strip_and_upper_case = lambda s: s.strip().upper()
+print(strip_and_upper_case(" Hello "))
+greeting = lambda x, *args, **kwargs: print(x, args, kwargs)
+greeting('hello', 'world', world = 'world')
+def foo(msg):    
+    print(msg) 
+greet = lambda x = "hello world": foo(x) 
+greet()
+def make(action = 'nothing'):
+    return action
+print(make("fun"))
+print(make(action = "sleep"))
+print(make())
+
+def give_me_five():   
+    return 5
+print(give_me_five()) 
+print(give_me_five() + 10)
+def give_me_another_five():    
+    return 5    
+    print('This statement will not be printed. Ever.')
+print(give_me_another_five())
+def give_me_two_fives():   
+    return 5, 5  
+first, second = give_me_two_fives()
+print(first, second) 
+def makeInc(x):  
+    def inc(y):
+        return y + x  
+    return inc
+incOne = makeInc(1)
+incFive = makeInc(5)
+print(incOne(5))
+print(incFive(5))
+def makeInc(x):  
+    def inc(y): 
+        nonlocal x
+        x += y  
+        return x 
+    return inc
+incOne = makeInc(1) 
+print(incOne(5))
+def makeInc(x): 
+    def inc(y):     
+        nonlocal x   
+        x += y       
+        return x  
+    return inc 
+incOne = makeInc(1) 
+print(incOne(5))
+def fibonacci(n):    
+    def step(a,b):        
+        return b, a+b    
+    a, b = 0, 1    
+    for i in range(n):       
+        a, b = step(a, b) 
+    return a
+def make_adder(n):    
+    def adder(x):        
+        return n + x   
+    return adder
+add5 = make_adder(5)
+add6 = make_adder(6) 
+print(add5(10), add6(10))
+def repeatedly_apply(func, n, x):    
+    for i in range(n):      
+        x = func(x)   
+    return x 
+repeatedly_apply(add5, 5, 1)
+def cursing(depth): 
+    try:   
+        cursing(depth + 1) 
+    except RuntimeError as RE:    
+        print('I recursed {} times!'.format(depth))
+        
+cursing(0)
+lambda_factorial = lambda i : 1 if i==0 else i* lambda_factorial(i - 1) 
+print(lambda_factorial(4))
+def factorial(n):    
+    if n == 0:       
+        return 1    
+    else:       
+        return n * factorial(n - 1)
+print(factorial(3))
+factorial = lambda n : 1 if n == 0 else n * factorial(n - 1)
+
+def divide(dividend, divisor):
+    print(dividend / divisor)
+print(divide(10, 2))
+print(divide(divisor = 2, dividend = 10))
+
+def unpacking(a, b, c = 45, d = 60, *args, **kwargs):
+    print(a, b, c, d, args, kwargs)
+unpacking(1, 2)
+unpacking(1, 2, 3, 4)
+unpacking(1, 2, c = 3, d = 4)
+unpacking(1, 2, d = 4, c = 3)
+pair = (3,)
+unpacking(1, 2, *pair, d = 4)
+unpacking(1, 2, d = 4, *pair)
+args_list = [3]
+unpacking(1, 2, *args_list, d = 4)
+unpacking(1, 2, d = 4, *args_list)
+pair = (3, 4)
+unpacking(1, 2, *pair)
+unpacking(1, 2, 3, 4, *pair)
+args_list = [3, 4]
+unpacking(1, 2, *args_list)
+unpacking(1, 2, 3, 4, *args_list)
+arg_dict = {'c' : 3, 'd' : 4}
+unpacking(1, 2, **arg_dict)
+arg_dict = {'d' : 4, 'c' : 3}
+unpacking(1, 2, **arg_dict)
+arg_dict = {'c' : 3, 'd' : 4, 'not_a_parameter' : 75}
+unpacking(1, 2, **arg_dict)
+def to_percent(num):   
+    return num * 100
+print(list(map(to_percent, [0.95, 0.75, 1.01, 0.1])))
+
+from functools import partial 
+from operator import mul
+rate = 0.9
+dollars = {'under_my_bed': 1000,           'jeans': 45, 'bank': 5000}
+print(sum(map(partial(mul, rate), dollars.values())))
+def average(*args):   
+    return float(sum(args)) / len(args) 
+measurement1 = [100, 111, 99, 97] 
+measurement2 = [102, 117, 91, 102] 
+measurement3 = [104, 102, 95, 101]
+print(list(map(average, measurement1, measurement2, measurement3)))
+def func(myList):   
+    for item in myList:       
+        print(item)
+func([1, 2, 3, 5, 7])
+lst = ['a', 'b', 'c', 'd'] 
+func(lst)
+def median_of_three(a, b, c):    
+    return sorted((a, b, c))[1]
+print(list(map(median_of_three, measurement1, measurement2, measurement3, measurement3)))
+import operator 
+measurement1 = [100, 111, 99, 97] 
+measurement2 = [102, 117]
+print(list(imap(operator.sub, measurement1, measurement2)))
+print(list(imap(operator.sub, measurement2, measurement1)))
+print(list(map(operator.sub, measurement1, measurement2)))
+print(list(map(operator.sub, measurement2, measurement1)))
+
+insects = ['fly', 'ant', 'beetle', 'cankerworm']
+f = lambda x : x + ' is an insect' print(list(map(f, insects)))
+print(list(map(len, insects)))
+
+carnivores = ['lion', 'tiger', 'leopard', 'arctic fox'] 
+herbivores = ['african buffalo', 'moose', 'okapi', 'parakeet'] 
+omnivores = ['chicken', 'dove', 'mouse', 'pig']
+def animals(w, x, y, z):    
+    return '{0}, {1}, {2}, and {3} ARE ALL ANIMALS'.format(w.title(), x, y, z)
+import pprint pprint.pprint(list(map(animals, insects, carnivores, herbivores, omnivores)))
