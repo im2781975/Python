b3196f95a51566b0a1fb5fefc9e1cd8b36b72621
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-01-09 07:21:48 +0600

    Update 1.py

--- Geek/Oop/1.py
+++ Geek/Oop/1.py
@@ -17,3 +17,43 @@ Rodger = Dog()
 
 print(Rodger.attr1)
 Rodger.fun()
+
+class MyError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return(repr(self.value))
+try:
+    raise(MyError(3*2))
+except MyError as error:
+    print('A New Exception occurred: ', error.value)
+
+class Error(Exception):
+    pass
+class zerodivision(Error):
+    pass
+try:
+    i_num = int(input("Enter a number: "))
+    if i_num == 0:
+        raise zerodivision
+except zerodivision:
+    print("Input value is zero, try again!")
+    print()
+class Error(Exception):
+    pass
+class TransitionError(Error):
+    def __init__(self, prev, nex, msg):
+        self.prev = prev
+        self.next = nex
+        self.msg = msg
+try:
+    raise(TransitionError(2, 3*2, "Not Allowed"))
+except TransitionError as error:
+    print('Exception occurred: ', error.msg)
+class Networkerror(RuntimeError):
+    def __init__(self, arg):
+        self.args = arg
+try:
+    raise Networkerror("Error")
+except Networkerror as e:
+    print(e.args)
