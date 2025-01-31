14bb88ecd7c327f4d48f1f50ea5b808bdf9698bc
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-01-31 09:29:25 +0600

    Create .py

---
+++ Assingment/.py
@@ -0,0 +1,21 @@
+# split a balanced string which consists ['R', 'L'] into maximum number of 
+# strings such that the new strings are also balanced.
+def split(ing):
+    cnt = cntX = cntY = 0
+    balanced = []
+    for ch in ing:
+        if ch == 'L':
+            cntX += 1
+        elif ch == 'R':
+            cntY += 1
+        if cntX == cntY:
+            balanced.append(ing[:cntX + cntY])
+            cnt += 1
+            ing = ing[cntX + cntY:]
+            cntX = cntY = 0
+    return cnt, balanced
+ing = input().strip()
+cnt, balanced = split(ing)
+print(cnt)
+for rin in balanced:
+    print(rin, end = " ")
