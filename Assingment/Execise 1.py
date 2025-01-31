c87052cf93b34b737f958c6ea9514ec550662dd1
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-01-31 11:36:34 +0600

    Update Execise 1.py

--- Assingment/Execise 1.py
+++ Assingment/Execise 1.py
@@ -524,5 +524,29 @@ while GuessNumber != answer:
         break
     elif GuessNumber != answer:
         print("Guess again")
-        
-        
+#Hangman
+import random
+word_list = ["apple", "beautiful", "potato"]
+live = 6
+choosen_word = random.choice(word_list)
+print(choosen_word)
+display = []
+for i in range(len(choosen_word)):
+    display += '_'
+print(display)
+Game_Over = False
+while not Game_Over:
+    guess_letter = input("Guess a letter: ").lower()
+    for i in range(len(choosen_word)):
+        letter = choosen_word[i]
+        if letter == guess_letter:
+            display[i] = guess_letter
+    print(display)
+    if guess_letter not in choosen_word:
+        live -= 1
+        if live == 0:
+            Game_Over = True
+            print("You Lose")
+        if '_' not in display:
+            Game_Over = True
+            print("You win")
