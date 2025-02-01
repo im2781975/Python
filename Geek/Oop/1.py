a942f153cd44524c45516610deb6024efd6f7246
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-02-01 21:09:48 +0600

    Update 1.py

--- Geek/Oop/1.py
+++ Geek/Oop/1.py
@@ -1,55 +1,61 @@
-#class keyword
 class Dog:
-
-    attr1 = "mammal"
-
-    attr2 = "dog"
- 
-
-    def fun(self):
-
-        print("I'm a", self.attr1)
-
-        print("I'm a", self.attr2)
- 
-
-Rodger = Dog()
-
-print(Rodger.attr1)
-Rodger.fun()
-
+    x = "molla"; y = "vai"
+    def func(self):
+        print("i am ", self.x)
+        print("i am ", self.y)
+    
+dog = Dog()
+print(dog.x, dog.y); dog.func()
+"""                    """
 class MyError(Exception):
     def __init__(self, value):
         self.value = value
     def __str__(self):
-        return(repr(self.value))
+        return f"MyError has occurred with value: {repr(self.value)}"
 try:
-    raise(MyError(3*2))
+    raise MyError(3 * 2)
 except MyError as error:
-    print('A New Exception occurred: ', error.value)
-
+    print('A New Exception occurred:', error.value)
+    print('Error details:', str(error))
+finally:
+    print('Exception handling completed.')
+"""                    """
 class Error(Exception):
     pass
 class zerodivision(Error):
     pass
-try:
-    i_num = int(input("Enter a number: "))
-    if i_num == 0:
-        raise zerodivision
-except zerodivision:
-    print("Input value is zero, try again!")
-    print()
+while True:
+    try:
+        num = int(input("Enter a number (or type 'exit' to quit): "))
+        if num == 0:
+            raise zerodivision
+        print(f"You entered: {num}")
+        break 
+    except zerodivision:
+        print("Input value is zero, try again!\n")
+    except ValueError:
+        print("Invalid input! Please enter a valid integer.\n")
+    except KeyboardInterrupt:
+        print("\nProgram interrupted. Exiting...")
+        break
+"""                    """
 class Error(Exception):
+    """Base class for other exceptions."""
     pass
 class TransitionError(Error):
+    """Raised when an operation is not allowed due to an invalid state transition."""
     def __init__(self, prev, nex, msg):
         self.prev = prev
-        self.next = nex
+        self.nex = nex
         self.msg = msg
+    def __str__(self):
+        return f"Transition from {self.prev} to {self.nex} is not allowed: {self.msg}"
 try:
-    raise(TransitionError(2, 3*2, "Not Allowed"))
+    raise TransitionError(2, 3 * 2, "Not Allowed")
 except TransitionError as error:
-    print('Exception occurred: ', error.msg)
+    print('Exception occurred:', error.msg)
+    print('Full Error Details:', error)
+"""                        """
 class Networkerror(RuntimeError):
     def __init__(self, arg):
         self.args = arg
