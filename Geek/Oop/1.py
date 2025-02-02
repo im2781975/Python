a7b1a316fb18b18c5c68d5bb0e10d47d3979e6c9
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-02-02 14:14:20 +0600

    Update 1.py

--- Geek/Oop/1.py
+++ Geek/Oop/1.py
@@ -168,6 +168,21 @@ l = lion(); l.move();
 print(issubclass(lion, animal))
 print(isinstance(lion(), animal))
 """                    """
+from abc import ABC, abstractmethod
+class Animal(ABC):
+    @abstractmethod
+    def sound(self):
+        pass
+class Dog(Animal):
+    def sound(self):
+        return "Bark"
+class Cat(Animal):
+    def sound(self):
+        return "Meow"
+animals = [Dog(), Cat()]
+for animal in animals:
+    print(animal.sound())
+"""					"""
 from abc import ABC
 class B(ABC):
     def D(self):
