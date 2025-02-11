1a8e217299a63b87a15ff751d94b517e7e594154
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-02-11 12:39:32 +0600

    Create Panda.py

---
+++ Panda.py
@@ -0,0 +1,84 @@
+import pandas as pd 
+import numpy as np
+ser = pd.Series() 
+print("Pandas Series: ", ser) 
+data = np.array(['g', 'e', 'e', 'k', 's']) 
+ser = pd.Series(data) 
+print("Pandas Series:\n", ser)
+
+import pandas as pd 
+  
+# Calling DataFrame constructor 
+df = pd.DataFrame() 
+print(df)
+
+# list of strings 
+lst = ['Geeks', 'For', 'Geeks', 'is', 'portal', 'for', 'Geeks'] 
+  
+# Calling DataFrame constructor on list 
+df = pd.DataFrame(lst) 
+print(df)
+
+import pandas as pd
+import pandas as pd
+ 
+# Calling DataFrame constructor
+df = pd.DataFrame()
+ 
+print(df)
+
+import pandas as pd
+import pandas as pd
+ 
+# list of strings
+lst = ['Geeks', 'For', 'Geeks', 'is', 
+            'portal', 'for', 'Geeks']
+ 
+# Calling DataFrame constructor on list
+df = pd.DataFrame(lst)
+print(df)
+
+import pandas as pd
+ 
+# initialise data of lists.
+data = {'Name':['Tom', 'nick', 'krish', 'jack'], 'Age':[20, 21, 19, 18]}
+ 
+# Create DataFrame
+df = pd.DataFrame(data)
+ 
+# Print the output.
+print(df)
+import pandas as pd
+ 
+# dictionary of lists
+dict = {'name':["aparna", "pankaj", "sudhir", "Geeku"],
+        'degree': ["MBA", "BCA", "M.Tech", "MBA"],
+        'score':[90, 40, 80, 98]}
+ 
+df = pd.DataFrame(dict)
+ 
+print(df)
+
+import pandas as pd
+
+data = {'Name': ['John', 'Alice', 'Bob', 'Eve', 'Charlie'],
+        'Age': [25, 30, 22, 35, 28],
+        'Gender': ['Male', 'Female', 'Male', 'Female', 'Male'],
+        'Salary': [50000, 55000, 40000, 70000, 48000]}
+
+df = pd.DataFrame(data)
+print(df.index)  # Accessing the index
+
+# Reset the index back to the default integer index
+df_reset = df.reset_index()
+print(df_reset)
+# Reset the index back to the default integer index
+df_reset = df.reset_index()
+print(df_reset)
+row = df.loc['Alice']
+print(row)
+# Set 'Age' as the new index
+df_with_new_index = df.set_index('Age')
+print(df_with_new_index)
+
+
