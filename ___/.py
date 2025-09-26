13978a5ab4a266c1f266e7f0293115d359347a32
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-09-26 09:01:34 +0200

    Update .py

--- ___/.py
+++ ___/.py
@@ -1,4 +1,15 @@
+#string is immutable like a[0] = 'R' cant assign
 s = """He'l" l\'"""
+a, b, c, d = 'Molla vai', 'Molla\'s vai', "Molla's vai", """ibrahim 
+molla """
+print(a, b, c, d)
+print(a.upper(), a.lower(), a.capitalize())
+print(a,'\n',f"a[3]: {a[3]}\na[2 : 6]: {a[2:6]}\na[1 : 8]: {a[1 : 8]}")
+print(f"a[8 : 0 : -1]: {a[8 : 0 : -1]}\na[-1 : -8: -1]: {a[-1 : -12 : -1]}")
+for char in a:
+    print(char, end = ' ')
+if 'molla' in a: print("Exits")
+else: print("Not exits")
 #repr() : single quote (') in the string is escaped as \'.
 print(s + "\n" + repr(s) + "\n" + str(s))
 print(eval(repr(s)) + "\n" + eval(repr(s)) == s)
@@ -16,4 +27,3 @@ print("concatenation of str is:", first + sec, ",", a + b)
 text = a + b #loop
 for char in text:
     print(char, end = ' ')
- 
