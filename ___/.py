b7c81a175328cf12f7a8bd4b409924fb1dd9cd0f
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-09-26 06:47:52 +0200

    Update .py

--- ___/.py
+++ ___/.py
@@ -2,8 +2,18 @@ s = """He'l" l\'"""
 #repr() : single quote (') in the string is escaped as \'.
 print(s + "\n" + repr(s) + "\n" + str(s))
 print(eval(repr(s)) + "\n" + eval(repr(s)) == s)
-###                ###
+
 ing = "HELLO"
 rev = reversed(ing); print(rev)
 rev = "".join(reversed(ing)); print(rev)
 print("HELLO"[::-1])
+
+first = input("Enter: "); sec = input("Enter: ")
+tmp = first; first = sec; sec = tmp; #swap
+print("swap: ", first, sec);
+a, b = "molla", "vai"
+print("concatenation of str is:", first + sec, ",", a + b)
+text = a + b #loop
+for char in text:
+    print(char, end = ' ')
+ 
