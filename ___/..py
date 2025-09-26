13f27eaa322fb1746dd7421218a6dcf675c6741c
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-09-26 08:27:54 +0200

    Update ..py

--- ___/..py
+++ ___/..py
@@ -1,23 +1,4 @@
-s = """w'o"w"""
-#repr() : single quote (') in the string is escaped as \'.
-print(repr(s), str(s))
-print(eval(repr(s)))
-print(eval(repr(s)) == s)
 
-ing = "Hello"
-rev = reversed("Hello")
-print(rev)
-
-#concatenate string
-first = input("Enter first:")
-sec = input("Enter second: ")
-print(first + sec)
-a, b = "molla", "vai"
-print("concatenate a & b: ",a + b)
-
-text = "ibrahim"
-for char in text:
-    print(char)
 
 name = input("whats your name: ")
 length = len(name)
