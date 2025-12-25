018536b216f4774c3cc40463b6dd741311df2659
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-12-25 13:34:37 +0100

    Update 1.js

--- Devo/1.js
+++ Devo/1.js
@@ -13,3 +13,25 @@
     </script>
   </body>
 </html>
+<!DOCTYPE HTML>
+<html>
+  <head></head>
+  <body>
+    <div id = "main"> 
+      <h1> PISQR </h1>
+    </div>
+    <h1> 1 </h1>
+    <div class = "base"> PHARMACY </div>
+    <h1> 2 </h1>
+    <div class = "base"> THYSQR </div>
+  </body>
+  <script type = "text/javascript">
+    // var x = documet.getElementById("main").innerHTML
+    // console.log(x)
+    // document.getElementById("main").innerHTML = "<h1>PISQRE</h1>";
+    var x = document.getElementsByClassName("base")[0].innerHTML;
+    var x = document.getElementsByClassName("base")[1].innerHTML;
+    var x = document.getElementsByClassName("base")[1].innerHTML = "PHARMA"
+    // alert(x)
+  </script>
+</html>
