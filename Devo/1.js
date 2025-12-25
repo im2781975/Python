d3e2db7a5744a41e52ac1bafd591aa5d300f046d
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-12-25 11:53:45 +0100

    Create 1.js

---
+++ Devo/1.js
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+  <head></head>
+  <body>
+    <script type = "text/javascript">
+      // alert("Error")
+      // console.log("Gama Tech")
+      // document.write("<h1> Hello </h1>")
+      // document.write("<h1> EveryOne! </h1>")
+      prompt("Enter sentence", "")
+      confirm("Do You want to login?")
+      var name = "wscube"; document.write(name)
+    </script>
+  </body>
+</html>
