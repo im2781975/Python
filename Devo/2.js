b8044d9ad80435de456998fbb45be5e0ed03db57
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-12-28 10:14:21 +0100

    Update 2.js

--- Devo/2.js
+++ Devo/2.js
@@ -15,3 +15,43 @@
     }
   </script>
 </html>
+<!DOCTYPE HTML>
+<html>
+  <head></head>
+  <body>
+    <input type = "number" name = "first" id = "first" step = "any"/>
+    <select id = "ope">
+      <option value = "+"> + </option>
+      <option value = "-"> - </option>
+      <option value = "*"> * </option>
+      <option value = "/"> / </option>
+      <option value = "%"> ℅ </option>
+    </select>
+    <input type = "number" name = "duo" id = "duo" step = "any"/>
+    <button onclick = "cal()"> calculate </button>
+    <input type = "text" name = "" id = "res" readonly = ""/>
+  </body>
+  <script type = "text/javascript">
+    function cal(){
+      var op1 = document.getElementById('first').value;
+      var op2 = document.getElementById('duo').value;
+      var ope = document.getElementById('ope').value;
+      if (ope == '+'){
+        var res = parseInt(op1) + parseInt(op2);
+      }
+      if (ope == '-'){
+        var res = parseInt(op1) - parseInt(op2);
+      }
+      if (ope == '*'){
+        var res = parseInt(op1) * parseInt(op2);
+      }
+      if (ope == '/'){
+        var res = parseInt(op1) / parseInt(op2);
+      }
+      if (ope == '%'){
+        var res = parseInt(op1) % parseInt(op2);
+      }
+      document.getElementById('res').value = res;
+    }
+  </script>
+</html>
