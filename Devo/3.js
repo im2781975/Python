4c6a0128e7d7caf38ab64dc819962cc48ec7e5c7
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-12-31 10:31:23 +0100

    Create 3.js

---
+++ Devo/3.js
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML>
+<html>
+  <head></head>
+  <body>
+    <h1 id = "res"></h1>
+    <button onclick = "getDateTime()"> DateTime </button>
+    <button onclick = "getYear()"> Year </button>
+    <button onclick = "getMonth()"> Month </button>
+    <button onclick = "getHour()"> Hour </button>
+    <button onclick = "getDate()"> Date </button>
+    <button onclick = "getmin()"> Minute </button>
+    <button onclick = "getsec()"> Second </button>
+    <button onclick = "getmilisec()"> Milisec </button>
+    <button onclick = "toTime()"> Time </button>
+    <button onclick = "toDay()"> Day </button>
+  </body>
+  <script>
+    var date = new Date()
+    function getDateTime(){
+      document.getElementById('res').innerText = date;
+    }
+    function getYear(){
+      document.getElementById('res').innerText = date.getFullYear();
+    }
+    function getMonth(){
+      var month = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Set", "Oct", "Nov", "Dec"]
+      document.getElementById('res').innerText = month[date.getMonth()];
+    }
+    function getHour(){
+      document.getElementById('res').innerText = date.getHours();
+    }
+    function getDate(){
+      document.getElementById('res').innerText = date.getDate();
+    }
+    function getmin(){
+      document.getElementById('res').innerText = date.getMinutes();
+    }
+    function getsec(){
+      document.getElementById('res').innerText = date.getSeconds();
+    }
+    function getmilisec(){
+      document.getElementById('res').innerText = date.getMilliseconds();
+    }
+    function toTime(){
+      document.getElementById('res').innerText = date.getTime();
+    }
+    function toDay(){
+      document.getElementById('res').innerText = date.getDay();
+    }
+  </script>
+</html>
