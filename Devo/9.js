c3914c788b85bcdd82fd7af22266b0aa3355aea1
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-02-06 08:51:19 +0600

    Create 9.js

---
+++ Devo/9.js
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+  <head></head>
+  <body>
+      <button onclick = "task()"> Click </button>
+      <h6 id = 'res'></h6>
+      <input type = "" name = "" id = "num"/>
+      <button onclick = "text()"> Press </button>
+      <h2 id = 'ans'></h2>
+  </body>
+  <script type = "text/javascript">
+    function task() {
+      try {
+        // alertf("YoBro");
+        var val = "ws"; // 9 | ws
+        var x = val.toUpperCase();
+        document.getElementById('res').innerText = x;
+      }
+      catch(err) {
+        document.getElementById('res').innerText = err
+      }
+    }
+    var num = document.getElementById('num');
+    function text() {
+      var val = num.value;
+      // alert(val);
+      try {
+        if(isNaN(val)) {
+          throw "this isn't a number";
+        }
+        else {
+          if(val < 5 || val > 10) {
+            throw "OutOfBound";
+          }
+          else {
+            throw "";
+          }
+        }
+      }
+      catch(ERM) {
+        document.getElementById("ans").innerText = ERM;
+      }
+      finally {
+        num.value = "";
+      }
+    }
+  </script>
+</html>
