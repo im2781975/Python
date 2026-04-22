79a0357684c7949f67ef444ed44f705cb92ea942
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-04-22 10:05:29 +0200

    Create 1.js

---
+++ UI/Js/1.js
@@ -0,0 +1,14 @@
+let a = 45.9
+console.log(a); console.log(typeof a);
+let b = -1 / 0;
+console.log(b); console.log(typeof b);
+let c = "Hello" / 2;
+console.log(c); console.log(typeof c);
+let d = 9876543211234567890123n;
+let e = 9876543311234567890123n;
+console.log(d + e); console.log(typeof d);
+// bigint & int can't perform any arithmetic operation
+console.log("abcde");
+let iscontain = true; 
+console.log(iscontain); console.log(typeof iscontain)
+let f; console.log(f);
