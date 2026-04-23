c5451cf911aa1d0ca792dd5aeebac7f4a5101871
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-04-23 12:58:02 +0200

    Update 1.js

--- UI/Js/1.js
+++ UI/Js/1.js
@@ -8,7 +8,39 @@ let d = 9876543211234567890123n;
 let e = 9876543311234567890123n;
 console.log(d + e); console.log(typeof d);
 // bigint & int can't perform any arithmetic operation
-console.log("abcde");
+let e1 = "abcd"; console.log(e1);
 let iscontain = true; 
 console.log(iscontain); console.log(typeof iscontain)
 let f; console.log(f);
+let g = 97; let h = String(g);
+console.log(typeof h);
+let i = String(iscontain); console.log(typeof i);
+let j = Number(e1); console.log(typeof j);
+let k = "567TAP"; // "TAP567", " 123TAP", " 1 2 3"
+let l = parseInt(k);
+console.log(typeof(l)); console.log(l);
+console.log(Boolean(NaN));// 0, "", [], null
+console.log(Boolean(-Infinity));
+// compare
+let age = 18; console.log(age == 18);
+console.log("ibrahim" == "Ibrahim")
+console.log(5 == "5"); console.log(5 === '5')
+console.log(true == '1');console.log(undefined == null);
+console.log('' == 0); console.log(false == '')
+console.log(null == false); console.log(undefined == 0);
+console.log(NaN == NaN);
+console.log(Number(true) == Number('1'));
+console.log('01' == 1); console.log('010' == 10);
+// control statement
+let ag = 20;
+if(ag < 12) { console.log("child"); }
+else if(ag < 65){ console.log("adult"); }
+else{ console.log("senior");}
+let isvip = true;
+if(isvip){ console.log("welcome"); }
+else { console.log("Go Away"); }
+
+let balance = 1000, withdraw = 700;
+if(balance >= withdraw){ balance -= withdraw;}
+else{ console.log("Insufficient"); }
+console.log(balance);
